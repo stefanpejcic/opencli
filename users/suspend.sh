1d75578d12683207ed123c6ac0749482b3d51ea5
Stefan Pejcic <stefan@pejcic.rs>
2023-09-20 11:28:02 +0200

    Update suspend.sh

--- users/suspend.sh
+++ users/suspend.sh
@@ -9,16 +9,12 @@ fi
 # Get username from command-line argument
 username="$1"
 
-# Function to pause (suspend) a user
-pause_user() {
-    # Pause the Docker container
-    docker stop "$username"
 
-    # Add a suspended timestamp prefix to the username in the database
-    suspended_username="SUSPENDED_$(date +"%Y%m%d%H%M%S")_$username"
-    
-    # MySQL database configuration (same as in your original script)
-    config_file="config.json"
+#########################################################################
+############################### DB LOGIN ################################ 
+#########################################################################
+    # MySQL database configuration
+    config_file="/usr/local/admin/db.cnf"
 
     # Check if the config file exists
     if [ ! -f "$config_file" ]; then
@@ -26,18 +22,27 @@ pause_user() {
         exit 1
     fi
 
-    # Read MySQL login credentials from the JSON configuration file
-    mysql_user=$(jq -r .mysql_user "$config_file")
-    mysql_password=$(jq -r .mysql_password "$config_file")
-    mysql_database=$(jq -r .mysql_database "$config_file")
+    mysql_database="panel"
+
+#########################################################################
+
+
+
+# Function to pause (suspend) a user
+pause_user() {
+    # Pause the Docker container
+    docker stop "$username"
+
+    # Add a suspended timestamp prefix to the username in the database
+    suspended_username="SUSPENDED_$(date +"%Y%m%d%H%M%S")_$username"
 
     # Update the username in the database with the suspended prefix
     mysql_query="UPDATE users SET username='$suspended_username' WHERE username='$username';"
     
-    mysql -u "$mysql_user" -p"$mysql_password" -D "$mysql_database" -e "$mysql_query"
+    mysql --defaults-extra-file=$config_file -D "$mysql_database" -e "$mysql_query"
 
     if [ $? -eq 0 ]; then
-        echo "User '$username' paused (suspended) successfully with username '$suspended_username' in MySQL database."
+        echo "User '$username' paused (suspended) successfully."
     else
         echo "Error: User pause (suspend) failed."
     fi
@@ -45,5 +50,3 @@ pause_user() {
 
 # Pause (suspend) the user
 pause_user
-
-echo "Script completed"
