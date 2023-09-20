05659b720a49d618c23df1326eb1fa0f0595a32f
Stefan Pejcic <stefan@pejcic.rs>
2023-09-20 13:31:16 +0200

    Update restore.sh

--- backup/restore.sh
+++ backup/restore.sh
@@ -1,5 +1,44 @@
-volume data overwrite
-docker rm i run image
+#!/bin/bash
+
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
+# Get the backup directory path from the first argument
+backup_dir="$1"
+
+# Check if a backup directory path is provided
+if [ -z "$backup_dir" ]; then
+  echo "Usage: $0 <backup_directory>"
+  exit 1
+fi
+
+# Extract the container_name from the backup directory path
+container_name=$(basename "$(dirname "$backup_dir")")
+volume_name="mysql-$container_name"
+backup_file="$backup_dir/docker_${container_name}_$(basename "$backup_dir").tar"
+sql_dump_file="$backup_dir/user_data_dump.sql"
+
+#echo $container_name
+#echo $volume_name
+#echo $backup_file
+#echo $sql_dump_file
+
+# Check if the backup files exist
+if [ ! -f "$backup_file" ] || [ ! -f "$sql_dump_file" ]; then
+  echo "${RED}ERROR${ENDCOLOR}: Backup files not found in the specified directory."
+  exit 1
+fi
+
+echo "Restoring Docker container $container_name..."
+docker import "$backup_file" "$container_name-image" || { echo "${RED}ERROR${ENDCOLOR}: Failed to restore Docker container $container_name"; exit 1; }
+#docker import /path/to/exampleimage.tgz
+docker volume create --name "$volume_name"
+
+# Copy data to the volume
+rsync -avR $backup_dir/mysql-volume/ /var/lib/docker/volumes/$volume_name/_data/
+
+RUN KOMANDA
 
 copy vhosts
 copy ssl
