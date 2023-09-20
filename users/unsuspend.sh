2b3242788f53c5e0ab122358230ef7f15451cee7
Stefan Pejcic <stefan@pejcic.rs>
2023-09-20 11:42:29 +0200

    Update unsuspend.sh

--- users/unsuspend.sh
+++ users/unsuspend.sh
@@ -9,11 +9,12 @@ fi
 # Get username from command-line argument
 username="$1"
 
-# Function to unpause (unsuspend) a user
-unpause_user() {
-    # Retrieve the suspended username from the database
-    # MySQL database configuration (same as in your original script)
-    config_file="config.json"
+
+#########################################################################
+############################### DB LOGIN ################################ 
+#########################################################################
+    # MySQL database configuration
+    config_file="/usr/local/admin/db.cnf"
 
     # Check if the config file exists
     if [ ! -f "$config_file" ]; then
@@ -21,13 +22,16 @@ unpause_user() {
         exit 1
     fi
 
-    # Read MySQL login credentials from the JSON configuration file
-    mysql_user=$(jq -r .mysql_user "$config_file")
-    mysql_password=$(jq -r .mysql_password "$config_file")
-    mysql_database=$(jq -r .mysql_database "$config_file")
+    mysql_database="panel"
 
+#########################################################################
+
+
+
+# Function to unpause (unsuspend) a user
+unpause_user() {
     # Query the database to get the suspended username
-    suspended_username=$(mysql -u "$mysql_user" -p"$mysql_password" -D "$mysql_database" -s -N -e "SELECT username FROM users WHERE username LIKE 'SUSPENDED_%' AND SUBSTRING(username, 11) = '$username';")
+    suspended_username=$(mysql --defaults-extra-file=$config_file -D "$mysql_database" -s -N -e "SELECT username FROM users WHERE username LIKE 'SUSPENDED_%stefan';")
 
     if [ -n "$suspended_username" ]; then
         # Remove the suspended timestamp prefix from the username
@@ -39,10 +43,10 @@ unpause_user() {
         # Update the username in the database without the suspended prefix
         mysql_query="UPDATE users SET username='$unsuspended_username' WHERE username='$suspended_username';"
     
-        mysql -u "$mysql_user" -p"$mysql_password" -D "$mysql_database" -e "$mysql_query"
+        mysql --defaults-extra-file=$config_file -D "$mysql_database" -e "$mysql_query"
 
         if [ $? -eq 0 ]; then
-            echo "User '$username' unpaused (unsuspended) successfully with username '$unsuspended_username' in MySQL database."
+            echo "User '$username' unsuspended successfully."
         else
             echo "Error: User unpause (unsuspend) failed."
         fi
