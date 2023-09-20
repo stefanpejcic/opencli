5fd5fcc99e23d7177d6799e8cee83262b62ff334
Stefan Pejcic <stefan@pejcic.rs>
2023-09-20 08:33:08 +0200

    Update change_username.sh

--- change_username.sh
+++ change_username.sh
@@ -1,4 +1,10 @@
 rename container
+
+get volume name
+docker inspect -f '{{range .Mounts}}{{.Name}} {{end}}' container_name_or_id
+
+
+
 rename volume
 change username in ufw rules
 change username in db
