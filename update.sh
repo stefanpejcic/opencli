07b4c0782a4449875a1b5e3010d6f3a381e867c6
Stefan Pejcic <stefan@pejcic.rs>
2023-10-25 17:59:10 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,42 +2,46 @@
 
 # Function to check if an update is needed
 check_update() {
+    local force_update=false
+
+    # Check if the '--force' flag is provided
+    if [[ "$1" == "--force" ]]; then
+        force_update=true
+        echo "Forcing updates, ignoring autopatch and autoupdate settings."
+    fi
+
     # Read the user settings from /usr/local/panel/conf/panel.config
-    local autopatch=$(awk -F= '/^autopatch=/{print $2}' /usr/local/panel/conf/panel.config)
-    local autoupdate=$(awk -F= '/^autoupdate=/{print $2}' /usr/local/panel/conf/panel.config)
-    #echo "Autoupdate: $autoupdate"
-    #echo "Autopatch: $autopatch"
+    local autopatch
+    local autoupdate
+
+    if [ "$force_update" = true ]; then
+        # When the '--force' flag is provided, set autopatch and autoupdate to "yes"
+        autopatch="yes"
+        autoupdate="yes"
+    else
+        autopatch=$(awk -F= '/^autopatch=/{print $2}' /usr/local/panel/conf/panel.config)
+        autoupdate=$(awk -F= '/^autoupdate=/{print $2}' /usr/local/panel/conf/panel.config)
+    }
+
     # Only proceed if autopatch or autoupdate is set to "yes"
-    if [ "$autopatch" = "yes" ] || [ "$autoupdate" = "yes" ]; then
+    if [ "$autopatch" = "yes" ] || [ "$autoupdate" = "yes" ] || [ "$force_update" = true ]; then
         # Run the update_check.sh script to get the update status
         local update_status=$(./update_check.sh)
 
         # Extract the local and remote version from the update status
         local local_version=$(echo "$update_status" | jq -r '.installed_version')
         local remote_version=$(echo "$update_status" | jq -r '.latest_version')
-        #echo "Installed version: $local_version"
-        #echo "Latest OpenPanel version: $remote_version"
 
-        # Check if autoupdate is "no"
-        if [ "$autoupdate" = "no" ] && [ "$local_version" \< "$remote_version" ]; then
-            # Extract the last two numbers of local and remote versions
-            local_version_minor=$(echo "$local_version" | awk -F. '{print $(NF-1) "." $NF}')
-            remote_version_last=$(echo "$remote_version" | awk -F. '{print $(NF-1) "." $NF}')
-        	#echo "local_version_minor: $local_version_last"
-        	#echo "remote_version_minor: $remote_version_minor"
-            # Compare the last two numbers of local and remote versions
-            if [ "$local_version_minor" != "$remote_version_minor" ]; then
-                echo "Update is available, autopatch will be installed."
-                # 
-                #  Run the update process
-                #
-                wget -q -O - https://update.openpanel.co/versions/$remote_version | bash
-            else
-                echo "No update required: $local_version -> $remote_version"
-            fi
+        # Check if autoupdate is "no" and not forcing the update
+        if [ "$autoupdate" = "no" ] && [ "$local_version" \< "$remote_version" ] && [ "$force_update" = false ]; then
+            echo "Update is available, autopatch will be installed."
+            # 
+            #  Run the update process
+            #
+            wget -q -O - https://update.openpanel.co/versions/$remote_version | bash
         else
-            # If autoupdate is "yes", check if local_version is less than remote_version
-            if [ "$local_version" \< "$remote_version" ]; then
+            # If autoupdate is "yes" or force_update is true, check if local_version is less than remote_version
+            if [ "$local_version" \< "$remote_version" ] || [ "$force_update" = true ]; then
                 echo "Update is available and will be automatically installed."
                 # 
                 # Run the update process
@@ -46,11 +50,11 @@ check_update() {
             else
                 echo "No update available."
             fi
-        fi
+        }
     else
         echo "Autopatch and Autoupdate are both set to 'no'. No updates will be installed automatically."
     fi
 }
 
-# Call the function to check for updates
-check_update
+# Call the function to check for updates, pass any additional arguments to it
+check_update "$@"
