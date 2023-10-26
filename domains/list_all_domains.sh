0690258241325409166f3aa195a18add1ffabef1
Stefan Pejcic <stefan@pejcic.rs>
2023-10-26 10:55:02 +0200

    Create list_all_domains.sh

---
+++ domains/list_all_domains.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+################################################################################
+# Script Name: list_all_domains.sh
+# Description: Lists all domain names hosted on the server
+#              Use: bash /usr/local/admin/scripts/domains/list_all_domains.sh
+# Author: Stefan Pejcic
+# Created: 26.10.2023
+# Last Modified: 26.10.2023
+# Company: openpanel.co
+# Copyright (c) openpanel.co
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+################################################################################
+
+
+# MySQL database configuration
+config_file="/usr/local/admin/db.cnf"
+mysql_database="panel"
+
+get_all_domains() {
+    
+    # Check if the config file exists
+    if [ ! -f "$config_file" ]; then
+        echo "Config file $config_file not found."
+        exit 1
+    fi
+    
+    # Query to fetch all domain names
+    all_domains="SELECT domain_url FROM domains"
+    domains=$(mysql --defaults-extra-file="$config_file" -D "$mysql_database" -e "$all_domains" -sN)
+    if [ -z "$domains" ]; then
+        echo "No domains found in the database."
+    else
+        echo "$domains"
+    fi
+}
+
+get_all_domains
