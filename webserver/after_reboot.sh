69fc07cd3992b04a32412d2ea3708f2d3b2680ff
Stefan Pejcic <stefan@pejcic.rs>
2023-11-01 18:17:00 +0100

    Update after_reboot.sh

--- webserver/after_reboot.sh
+++ webserver/after_reboot.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-# Stop Docker
-systemctl stop docker
-systemctl stop docker.socket
-systemctl stop containerd
-
-
 # Korisnici
 DOCKER_USERS=$(docker ps -a --format '{{.Names}}')
 
-# Start mount skriptu 
+# Start mount skriptu
 bash /usr/local/admin/scripts/webserver/mount_folders_for_all_users.sh
+
+# Stop servisa
+systemctl stop docker
+systemctl stop docker.socket
+systemctl stop containerd
+systemctl stop panel
+
+#Start Docker
+systemctl start docker
 
 # Loop kroz Docker usere i pokreni skript
 for USERNAME in $DOCKER_USERS; do
@@ -19,8 +22,9 @@ for USERNAME in $DOCKER_USERS; do
 
 done
 
-# Start Docker
-systemctl start docker
+#reset servisa
+service nginx reload
+service panel restart
 
 # Fix ports
 bash /usr/local/admin/scripts/webserver/fix_ufw_ports.sh
