571521e79be4277a0232e1473b8090e467e3415f
Stefan Pejcic <stefan@pejcic.rs>
2023-11-15 19:09:16 +0100

    Update and rename after_reboot.sh to on_startup.sh

---
+++ server/on_startup.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+################################################################################
+# Script Name: server/on_startup.sh
+# Description: Runs on system startup and configures files, docker and firewall.
+# Usage: opencli server-on_startup
+# Author: Stefan Pejcic
+# Created: 15.11.2023
+# Last Modified: 15.11.2023
+# Company: openpanel.co
+# Copyright (c) openpanel.co
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+################################################################################
+
+# Korisnici
+DOCKER_USERS=$(docker ps -a --format '{{.Names}}')
+
+# Start mount skriptu
+bash /usr/local/admin/scripts/files/remount_all.sh
+
+# Stop servisa
+systemctl stop docker
+systemctl stop docker.socket
+systemctl stop containerd
+systemctl stop panel
+
+#Start Docker
+systemctl start docker
+
+# Loop kroz Docker usere i pokreni skript
+for USERNAME in $DOCKER_USERS; do
+    # Run the user-specific script
+    bash /usr/local/admin/scripts/nginx/update_all_vhosts.sh $USERNAME
+
+done
+
+#reset servisa
+service nginx reload
+service panel restart
+
+# Fix ports
+bash /usr/local/admin/scripts/firewall/reset.sh
