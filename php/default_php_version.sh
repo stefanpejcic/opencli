98e6184002317acd9cf55267cbab47aee1549a9f
Stefan Pejcic <stefan@pejcic.rs>
2023-11-15 20:08:31 +0100

    Update default_php_version.sh

--- php/default_php_version.sh
+++ php/default_php_version.sh
@@ -1,4 +1,33 @@
 #!/bin/bash
+################################################################################
+# Script Name: php/default_php_version.sh
+# Description: View or change the default PHP version used for new domains added by user.
+# Usage: opencli php-default_php_version <username>
+#        opencli php-default_php_version <username> --update <new_php_version>
+# Author: Stefan Pejcic
+# Created: 07.10.2023
+# Last Modified: 15.11.2023
+# Company: openpanel.co
+# Copyright (c) openpanel.co
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+################################################################################
 
 # Function to update PHP version in the configuration file
 update_php_version() {
