0f8852d3b08e80c39654516ff8bc65a8afb6fb3b
Stefan Pejcic <stefan@pejcic.rs>
2023-11-15 20:10:26 +0100

    Update domain_php.sh

--- php/domain_php.sh
+++ php/domain_php.sh
@@ -1,4 +1,33 @@
 #!/bin/bash
+################################################################################
+# Script Name: php/domain_php.sh
+# Description: View or change the PHP version used for a single domain name.
+# Usage: opencli php-domain_php <domain_name>
+#        opencli php-domain_php <domain_name> --update <new_php_version>
+# Author: Stefan Pejcic
+# Created: 07.10.2023
+# Last Modified: 15.11.2023
+# Company: openpanel.co
+# Copyright (c) openpanel.co
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+################################################################################
 
 # Check if domain argument is provided
 if [ $# -lt 1 ]; then
