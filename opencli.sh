e56bb7da578532e2431db5bcf7117440f2b05d63
Stefan Pejcic <stefan@pejcic.rs>
2023-11-15 13:43:17 +0100

    Create opencli.sh

---
+++ opencli.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+################################################################################
+# Script Name: aliases.sh
+# Description: Makes all OpenCLI commands available on the terminal
+#              Use: opencli <COMMMAND-NAME>
+#              For a list of all available commands view alaises.txt file
+# Author: Stefan Pejcic
+# Created: 15.11.2023
+# Last Modified: 15.11.2023
+# Company: openpanel.co
+# Copyright (c) openpanel.co
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+################################################################################
+
+# Check if a command is provided
+if [ -z "$1" ]; then
+    echo "Usage: opencli <command>"
+    exit 1
+fi
+
+# Replace dashes with slashes
+COMMAND_WITH_SLASHES=$(echo "$1" | tr '-' '/')
+
+# Define the scripts directory
+SCRIPTS_DIR="/usr/local/admin/scripts"
+
+# Build the full path to the script
+SCRIPT_PATH="$SCRIPTS_DIR/$COMMAND_WITH_SLASHES.sh"
+
+# Check if the script exists
+if [ -e "$SCRIPT_PATH" ]; then
+    # Execute the script with the provided arguments
+    shift # remove the first argument (the script name)
+    bash "$SCRIPT_PATH" "$@"
+else
+    echo "Error: Command not found"
+    exit 1
+fi
