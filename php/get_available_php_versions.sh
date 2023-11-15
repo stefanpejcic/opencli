88831d46270b48a8020834d7deb081457da5fb74
Stefan Pejcic <stefan@pejcic.rs>
2023-11-15 20:13:18 +0100

    Update get_available_php_versions.sh

--- php/get_available_php_versions.sh
+++ php/get_available_php_versions.sh
@@ -1,4 +1,33 @@
 #!/bin/bash
+################################################################################
+# Script Name: php/get_available_php_versions.sh
+# Description: View or update the list of PHP versions that user can currently install.
+# Usage: opencli php-get_available_php_versions <username>
+#        opencli php-get_available_php_versions <username> --show
+# Author: Stefan Pejcic
+# Created: 07.10.2023
+# Last Modified: 15.11.2023
+# Company: openpanel.co
+# Copyright (c) openpanel.co
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+################################################################################
 
 # Initialize a flag to determine whether to show file content
 show_content=false
