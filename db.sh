c19600c986f2d572509d6e16764fdbcb466758f3
f1lk3 <152178979+f1lk3@users.noreply.github.com>
2023-11-27 13:55:29 +0100

    Create db.sh

---
+++ db.sh
@@ -0,0 +1,15 @@
+#########################################################################
+############################### DB LOGIN ################################ 
+#########################################################################
+    # MySQL database configuration
+    config_file="/usr/local/admin/db.cnf"
+
+    # Check if the config file exists
+    if [ ! -f "$config_file" ]; then
+        echo "Config file $config_file not found."
+        exit 1
+    fi
+
+    mysql_database="panel"
+
+#########################################################################
