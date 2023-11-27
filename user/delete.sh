b0cda2231c0fc36c478ca5c0f6b32a9413c4b099
f1lk3 <152178979+f1lk3@users.noreply.github.com>
2023-11-27 14:46:35 +0100

    db.sh

--- user/delete.sh
+++ user/delete.sh
@@ -65,22 +65,8 @@ confirm_action() {
     fi
 }
 
-#########################################################################
-############################### DB LOGIN ################################ 
-#########################################################################
-    # MySQL database configuration
-    config_file="/usr/local/admin/db.cnf"
-
-    # Check if the config file exists
-    if [ ! -f "$config_file" ]; then
-        echo "Config file $config_file not found."
-        exit 1
-    fi
-
-    mysql_database="panel"
-
-#########################################################################
-
+# DB
+source /usr/local/admin/scripts/db.sh
 
 
 
