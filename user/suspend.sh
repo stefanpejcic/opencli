e4d5cd69552a1cee6af9fad8a331a70e6f0ed4cf
f1lk3 <152178979+f1lk3@users.noreply.github.com>
2023-11-27 14:48:57 +0100

    db.sh

--- user/suspend.sh
+++ user/suspend.sh
@@ -37,23 +37,8 @@ fi
 # Get username from command-line argument
 username="$1"
 
-
-#########################################################################
-############################### DB LOGIN ################################ 
-#########################################################################
-    # MySQL database configuration
-    config_file="/usr/local/admin/db.cnf"
-
-    # Check if the config file exists
-    if [ ! -f "$config_file" ]; then
-        echo "Config file $config_file not found."
-        exit 1
-    fi
-
-    mysql_database="panel"
-
-#########################################################################
-
+# DB
+source /usr/local/admin/scripts/db.sh
 
 
 # Function to pause (suspend) a user
