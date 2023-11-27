cdb97d3043d16eeb390d5fc7b8f00bb3e5711c58
f1lk3 <152178979+f1lk3@users.noreply.github.com>
2023-11-27 14:46:54 +0100

    db.sh

--- user/unsuspend.sh
+++ user/unsuspend.sh
@@ -38,22 +38,8 @@ fi
 username="$1"
 
 
-#########################################################################
-############################### DB LOGIN ################################ 
-#########################################################################
-    # MySQL database configuration
-    config_file="/usr/local/admin/db.cnf"
-
-    # Check if the config file exists
-    if [ ! -f "$config_file" ]; then
-        echo "Config file $config_file not found."
-        exit 1
-    fi
-
-    mysql_database="panel"
-
-#########################################################################
-
+# DB
+source /usr/local/admin/scripts/db.sh
 
 
 # Function to unpause (unsuspend) a user
