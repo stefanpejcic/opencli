4c2da661b38336feffed55cfa80cff4f61173729
f1lk3 <152178979+f1lk3@users.noreply.github.com>
2023-11-27 14:47:41 +0100

    db.sh

--- user/change_plan.sh
+++ user/change_plan.sh
@@ -40,16 +40,8 @@ fi
 container_name=$1
 new_plan_id=$2
 
-# MySQL database configuration
-config_file="/usr/local/admin/db.cnf"
-
-# Check if the config file exists
-if [ ! -f "$config_file" ]; then
-    echo "Config file $config_file not found."
-    exit 1
-fi
-
-mysql_database="panel"
+# DB
+source /usr/local/admin/scripts/db.sh
 
 # Function to fetch the current plan ID for the container
 get_current_plan_id() {
