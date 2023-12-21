8542f678d28d7cd782534df0ae461d2058ad18eb
radovanjecmenica <147708271+radovanjecmenica@users.noreply.github.com>
2023-12-21 09:15:20 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -35,6 +35,7 @@ cron_jobs=(
   "0 */3 * * * certbot renew --post-hook 'systemctl reload nginx'"
   "0 1 * * * opencli backup-create"
   "15 0 * * * opencli update"
+  "30 2 * * * opencli domain-stats"
   "@reboot opencli server-on_startup"
 )
 
