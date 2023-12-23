a8b25ce4cbdeb2f315a448735af0d75a0c1c7ef1
Stefan Pejcic <stefan@pejcic.rs>
2023-12-23 09:47:23 +0100

    Update add.sh

--- user/add.sh
+++ user/add.sh
@@ -357,6 +357,7 @@ fi
 
 mkdir -p /usr/local/panel/core/stats/$username
 mkdir -p /usr/local/panel/core/users/$username
+touch /usr/local/panel/core/users/$username}/elastic.lock #elasticsearch is not installed
 echo "web_server: $web_server" > /usr/local/panel/core/users/$username/server_config.yml
 echo "default_php_version: $default_php_version" >> /usr/local/panel/core/users/$username/server_config.yml
 opencli php-get_available_php_versions $username
