2cfe0f0bf676d4233a4a6f3e36a0d088e0760b39
radovanjecmenica <147708271+radovanjecmenica@users.noreply.github.com>
2024-01-04 16:03:10 +0100

    Update rename.sh

--- user/rename.sh
+++ user/rename.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
 # Check if the correct number of arguments is provided
-if [ "$#" -ne 2 ]; then
+if [ "$#" -lt 2 ] || [ "$#" -gt 3 ]; then
     echo "Usage: $0 <old_username> <new_username>"
     exit 1
 fi
 
 old_username="$1"
 new_username="$2"
+DEBUG=false  # Default value for DEBUG
+
+# Parse optional flags to enable debug mode when needed!
+for arg in "$@"; do
+    case $arg in
+        --debug)
+            DEBUG=true
+            ;;
+        *)
+            ;;
+    esac
+done
 
 #1. check for forbidden usernames
 forbidden_usernames=("test" "restart" "reboot" "shutdown" "exec" "root" "admin" "ftp" "vsftpd" "apache2" "apache" "nginx" "php" "mysql" "mysqld" "www-data")
@@ -81,35 +93,60 @@ if docker ps -a --format '{{.Names}}' | grep -q "^${old_username}$"; then
 # ove treba za nginx 1 za apache 2 da se radi!!!
         
     # Execute commands inside the container
-    docker exec "$old_username" \
-        bash -c "usermod -l $new_username $old_username && \
-        sed -i 's#/home/$old_username#/home/$new_username#g' /etc/apache2/sites-available/* && \
-        sed -i 's#/home/$old_username#/home/$new_username#g' /etc/nginx/sites-available/* && \
-        service nginx reload && \
-        service apache2 reload"
-    
-    docker rename "$old_username" "$new_username"
-    # Rename the folder outside the container
+    if [ "$DEBUG" = true ]; then
+        docker exec "$old_username" \
+            bash -c "usermod -l $new_username $old_username && \
+            sed -i 's#/home/$old_username#/home/$new_username#g' /etc/apache2/sites-available/* && \
+            sed -i 's#/home/$old_username#/home/$new_username#g' /etc/nginx/sites-available/* && \
+            service nginx reload && \
+            service apache2 reload"
+        
+        docker rename "$old_username" "$new_username"
+        # Rename the folder outside the container
 
-    echo "Container renamed successfully."
+        echo "Container renamed successfully."
+    else
+            docker exec "$old_username" \
+            bash -c "usermod -l $new_username $old_username && \
+            sed -i 's#/home/$old_username#/home/$new_username#g' /etc/apache2/sites-available/* && \
+            sed -i 's#/home/$old_username#/home/$new_username#g' /etc/nginx/sites-available/* && \
+            service nginx reload && \
+            service apache2 reload" > /dev/null 2>&1
+        
+        docker rename "$old_username" "$new_username" > /dev/null 2>&1
+        # Rename the folder outside the container
+    fi
+
 else
     echo "Error: Container '$old_username' not found."
     exit 1
 fi
 
-mv /usr/local/panel/core/users/"$old_username" /usr/local/panel/core/users/"$new_username"
+if [ "$DEBUG" = true ]; then
+mv /usr/local/panel/core/users/"$old_username" /usr/local/panel/core/users/"$new_username" 
 rm /usr/local/panel/core/users/$new_username/data.json
+else
+mv /usr/local/panel/core/users/"$old_username" /usr/local/panel/core/users/"$new_username" > /dev/null 2>&1
+rm /usr/local/panel/core/users/$new_username/data.json > /dev/null 2>&1
+fi
 
 server_shared_ip=$(hostname -I 2>/dev/null | awk '{print $1}')
 json_file="/usr/local/panel/core/users/$new_username/ip.json"
 
-# Check if the JSON file for the user exists
-if [ -e "$json_file" ]; then
-    IP_TO_USE=$(jq -r '.ip' "$json_file")
-    echo "User has dedicated IP: $IP_TO_USE."
+if [ "$DEBUG" = true ]; then
+    if [ -e "$json_file" ]; then
+        IP_TO_USE=$(jq -r '.ip' "$json_file")
+        echo "User has dedicated IP: $IP_TO_USE."
+    else
+        IP_TO_USE="$server_shared_ip"
+        echo "User has no dedicated IP assigned, using shared IP address: $IP_TO_USE."
+    fi
 else
-    IP_TO_USE="$server_shared_ip"
-    echo "User has no dedicated IP assigned, using shared IP address: $IP_TO_USE."
+    if [ -e "$json_file" ]; then
+        IP_TO_USE=$(jq -r '.ip' "$json_file")
+    else
+        IP_TO_USE="$server_shared_ip"
+    fi
 fi
 
 
@@ -121,15 +158,26 @@ edit_nginx_files_on_host_server() {
     NGINX_CONF_PATH="/etc/nginx/sites-available"
     ALL_DOMAINS=$(opencli domains-user $USERNAME)
 
-    # Loop through Nginx configuration files for the user
-    for domain in $ALL_DOMAINS; do
-        DOMAIN_CONF="$NGINX_CONF_PATH/$domain.conf"
-        if [ -f "$DOMAIN_CONF" ]; then
-            # Update the server IP using sed
-            sed -i 's#/home/$old_username#/home/$NEW_USERNAME#g' "$DOMAIN_CONF"
-            echo "Username updated in $DOMAIN_CONF to $NEW_USERNAME."
-        fi
-    done
+    if [ "$DEBUG" = true ]; then
+        # Loop through Nginx configuration files for the user
+        for domain in $ALL_DOMAINS; do
+            DOMAIN_CONF="$NGINX_CONF_PATH/$domain.conf"
+            if [ -f "$DOMAIN_CONF" ]; then
+                # Update the server IP using sed
+                sed -i 's#/home/$old_username#/home/$NEW_USERNAME#g' "$DOMAIN_CONF"
+                echo "Username updated in $DOMAIN_CONF to $NEW_USERNAME."
+            fi
+        done
+    else
+        # Loop through Nginx configuration files for the user
+        for domain in $ALL_DOMAINS; do
+            DOMAIN_CONF="$NGINX_CONF_PATH/$domain.conf"
+            if [ -f "$DOMAIN_CONF" ]; then
+                # Update the server IP using sed
+                sed -i 's#/home/$old_username#/home/$NEW_USERNAME#g' "$DOMAIN_CONF" > /dev/null 2>&1
+            fi
+        done
+    fi
 
     # Restart Nginx to apply changes
     systemctl reload nginx
@@ -159,35 +207,58 @@ ports_opened=0
 # Delete exisitng rules for the old username
 update_firewall_rules() {
     IP_TO_USE=$1
-    # Delete existing rules for the specified user
-    ufw status numbered | awk -F'[][]' -v user="$old_username" '$NF ~ " " user "$" {gsub(/^[ \t]+|[ \t]+$/, "", $2); print $2}' | sort -rn | \
-    
-    while read -r rule_number; do
-        yes | ufw delete "$rule_number"
-    done
+    if [ "$DEBUG" = true ]; then
+        # Delete existing rules for the specified user
+        ufw status numbered | awk -F'[][]' -v user="$old_username" '$NF ~ " " user "$" {gsub(/^[ \t]+|[ \t]+$/, "", $2); print $2}' | sort -rn | \
+        
+        while read -r rule_number; do
+            yes | ufw delete "$rule_number"
+        done
 
-    # Loop through the container_ports array and open the ports in UFW if not already open
-    for port in "${container_ports[@]}"; do
-        host_port=$(extract_host_port "$port")
-    
-        if [ -n "$host_port" ]; then
-            # Open the port in UFW
-            echo "Opening port ${host_port} for port ${port} in UFW"
-            ufw allow to $IP_TO_USE port "$host_port" proto tcp comment "$new_username"
-            ports_opened=1
-        else
-            echo "Port ${port} not found in container"
+        # Loop through the container_ports array and open the ports in UFW if not already open
+        for port in "${container_ports[@]}"; do
+            host_port=$(extract_host_port "$port")
+        
+            if [ -n "$host_port" ]; then
+                # Open the port in UFW
+                echo "Opening port ${host_port} for port ${port} in UFW"
+                ufw allow to $IP_TO_USE port "$host_port" proto tcp comment "$new_username"
+                ports_opened=1
+            else
+                echo "Port ${port} not found in container"
+            fi
+        done
+
+        # Restart UFW if ports were opened
+        if [ $ports_opened -eq 1 ]; then
+            echo "Restarting UFW"
+            ufw reload
         fi
-    done
+    else
+        # Delete existing rules for the specified user
+        while read -r rule_number; do
+            yes | ufw delete "$rule_number" >/dev/null 2>&1
+        done < <(ufw status numbered | awk -F'[][]' -v user="$old_username" '$NF ~ " " user "$" {gsub(/^[ \t]+|[ \t]+$/, "", $2); print $2}' | sort -rn)
 
-    # Restart UFW if ports were opened
-    if [ $ports_opened -eq 1 ]; then
-        echo "Restarting UFW"
-        ufw reload
+        # Loop through the container_ports array and open the ports in UFW if not already open
+        for port in "${container_ports[@]}"; do
+            host_port=$(extract_host_port "$port")
+        
+            if [ -n "$host_port" ]; then
+                # Open the port in UFW
+                ufw allow to $IP_TO_USE port "$host_port" proto tcp comment "$new_username" >/dev/null 2>&1
+                ports_opened=1
+            else
+                # Empty else block
+                :
+            fi
+        done
+
+        # Restart UFW if ports were opened
+        if [ $ports_opened -eq 1 ]; then
+            ufw reload >/dev/null 2>&1
+        fi
     fi
-
-
-
 }
 
 # Function to rename user in the database
