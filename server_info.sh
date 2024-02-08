3468e93c20f5eb912f9a210f736c4687df9b73ef
Stefan Pejcic <stefan@pejcic.rs>
2024-02-08 14:59:38 +0100

    Update server_info.sh

--- server_info.sh
+++ server_info.sh
@@ -1,42 +1,52 @@
 #!/bin/bash
 
+# Create directory if it doesn't exist
+output_dir="/usr/local/admin/static/reports"
+mkdir -p "$output_dir"
+
+output_file="$output_dir/system_info_$(date +'%Y%m%d%H%M%S').txt"
+
+
 # Function to run a command and print its output with a custom message
 run_command() {
-  echo "# $2:"
-  $1
-  echo
+  echo "# $2:" >> "$output_file"
+  $1 >> "$output_file" 2>&1
+  echo >> "$output_file"
 }
 
 # Function to run OpenCLI commands if --cli flag is provided
 run_opencli() {
-  if [ "$cli_flag" = true ]; then
-    echo "=== OpenCLI Information ==="
+    echo "=== OpenCLI Information ===" >> "$output_file"
     run_command "opencli commands" "Available OpenCLI Commands"
-  fi
 }
 
-# Parse command line arguments
-cli_flag=false
-while [ "$#" -gt 0 ]; do
-  case "$1" in
-    --cli)
-      cli_flag=true
-      shift
-      ;;
-    *)
-      echo "Unknown option: $1"
-      exit 1
-      ;;
-  esac
-done
+# Function to display UFW rules if --ufw flag is provided
+run_ufw_rules() {
+  echo "=== Firewall Rules ===" >> "$output_file"
+  run_command "cat /etc/ufw/user.rules" "Server IPv4 Firewall Rules"
+}
 
+# Default values
+cli_flag=false
+ufw_flag=false
+
+# Parse command line arguments
+for arg in "$@"; do
+  if [ "$arg" = "--cli" ]; then
+    cli_flag=true
+  elif [ "$arg" = "--ufw" ]; then
+    ufw_flag=true
+  else
+    echo "Unknown option: $arg"
+    exit 1
+  fi
+done
 
 # Create directory if it doesn't exist
 output_dir="/usr/local/admin/static/reports"
 mkdir -p "$output_dir"
 
 # Collect system information
-echo "=== System Information ==="
 os_info=$(awk -F= '/^(NAME|VERSION_ID)/{gsub(/"/, "", $2); printf("%s ", $2)}' /etc/os-release)
 run_command "echo $os_info" "OS"
 run_command "uptime" "Uptime Information"
@@ -44,26 +54,26 @@ run_command "free -h" "Memory Information"
 run_command "df -h" "Disk Information"
 
 # Collect application information
-echo "=== Application Information ==="
 run_command "opencli v" "OpenPanel version"
 run_command "mysql --version" "MySQL Version"
 run_command "python3 --version" "Python Version"
 run_command "docker info" "Docker Information"
 
 # Run OpenCLI commands if --cli flag is provided
-run_opencli
+if [ "$cli_flag" = true ]; then
+  run_opencli
+fi
+
+# Display Firewall Rules if --ufw flag is provided
+if [ "$ufw_flag" = true ]; then
+  run_ufw_rules
+fi
 
 # Count the number of running Docker containers
 docker_container_count=$(docker ps -q | wc -l)
-echo "=== Number of Docker Containers ==="
-echo "Running Containers: $docker_container_count"
-
-# Save the information to a file
-output_file="$output_dir/system_info_$(date +'%Y%m%d%H%M%S').txt"
-exec > >(tee -a "$output_file") 2>&1
+echo "=== Number of Docker Containers ===" >> "$output_file"
+echo "Running Containers: $docker_container_count" >> "$output_file"
 
 # Print a message about the output file
-echo -e "\nInformation collected successfully. Please provide the following file to the support team:"
-echo "$output_file"
-
+echo -e "Information collected successfully. Please provide the following file to the support team:\n$output_file"
 exit 0
