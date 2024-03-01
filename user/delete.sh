1e89e982822b66e7a5a2fa7ab084bff62877fd69
Radovan Ječmenica <147708271+radovanjecmenica@users.noreply.github.com>
2024-03-01 14:22:12 +0100

    Update delete.sh

--- user/delete.sh
+++ user/delete.sh
@@ -143,12 +143,12 @@ delete_user_from_database() {
 # Function to disable UFW rules for ports containing the username
 disable_ports_in_ufw() {
   # Get the line numbers to delete
-  line_numbers=$(ufw status numbered | awk -v keyword="stefan" '$NF ~ keyword' | awk -F '[][]' '/\[/{print $2}')
+  line_numbers=$(ufw status numbered | awk -F'[][]' -v user="$username" '$NF ~ " " user "$" {gsub(/^[ \t]+|[ \t]+$/, "", $2); print $2}' |sort -rn)
                 #ufw status numbered | awk '$NF ~ /stefan/' | awk -F '[][]' '/\[/{print "[" $2 "]"}' | sed 's/[][]//g'
 
   # Loop through each line number and delete the corresponding rule
   for line_number in $line_numbers; do
-    ufw --force delete $line_number
+    yes | ufw delete $line_number
     echo "Deleted rule #$line_number"
   done
 }
