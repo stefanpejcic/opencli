055502bbf2398304b192efa43953007fc7defb43
Radovan Ječmenica <147708271+radovanjecmenica@users.noreply.github.com>
2024-04-10 14:19:31 +0200

    Update create.sh

--- plan/create.sh
+++ plan/create.sh
@@ -187,8 +187,10 @@ if [ "$docker_image" == "nginx" ]; then
   docker_image="openpanel_nginx"
 elif [ "$docker_image" == "litespeed" ]; then
   docker_image="openpanel_litespeed"
-else
+elif [ "$docker_image" == "apache" ]; then
   docker_image="openpanel_apache"
+else
+  docker_image="${10}"
 fi
 
 # Check if the plan name already exists in the database
