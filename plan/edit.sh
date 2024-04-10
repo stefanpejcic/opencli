90e5cf2dffb71c8d21e9939637ec5f1a134ac3ef
Stefan Pejcic <stefan@pejcic.rs>
2024-04-10 16:19:06 +0200

    Update edit.sh

--- plan/edit.sh
+++ plan/edit.sh
@@ -2,7 +2,7 @@
 ################################################################################
 # Script Name: plan/edit.sh
 # Description: Edit an existing hosting plan (Package) and modify its parameters.
-# Usage: opencli plan-edit old_plan_name new_plan_name new_description new_domains_limit new_websites_limit new_disk_limit new_inodes_limit new_db_limit new_cpu new_ram new_docker_image new_bandwidth new_storage_file
+# Usage: opencli plan-edit plan_id new_plan_name new_description new_domains_limit new_websites_limit new_disk_limit new_inodes_limit new_db_limit new_cpu new_ram new_docker_image new_bandwidth new_storage_file
 # Author: Radovan Jecmenica
 # Created: 10.04.2024
 # Last Modified: 10.04.2024
@@ -33,7 +33,7 @@ source /usr/local/admin/scripts/db.sh
 
 # Function to update values in the database
 update_plan() {
-  local old_plan_name="$1"
+  local plan_id="$1"
   local new_plan_name="$2"
   local description="$3"
   local domains_limit="$4"
@@ -60,13 +60,13 @@ fi
 ram="${ram}g"
   
   # Update the plan in the 'plans' table
-  local sql="UPDATE plans SET name='$new_plan_name', description='$description', domains_limit=$domains_limit, websites_limit=$websites_limit, disk_limit='$disk_limit', inodes_limit=$inodes_limit, db_limit=$db_limit, cpu=$cpu, ram='$ram', docker_image='$docker_image', bandwidth=$bandwidth, storage_file='$storage_file' WHERE name='$old_plan_name';"
+  local sql="UPDATE plans SET name='$new_plan_name', description='$description', domains_limit=$domains_limit, websites_limit=$websites_limit, disk_limit='$disk_limit', inodes_limit=$inodes_limit, db_limit=$db_limit, cpu=$cpu, ram='$ram', docker_image='$docker_image', bandwidth=$bandwidth, storage_file='$storage_file' WHERE id='$plan_id';"
 
   mysql --defaults-extra-file=$config_file -D "$mysql_database" -e "$sql"
   if [ $? -eq 0 ]; then
     docker network prune -f
   else
-    echo "Failed to update plan '$old_plan_name' to '$new_plan_name'"
+    echo "Failed to update plan id '$plan_id'"
     exit 1
   fi
 
@@ -95,20 +95,20 @@ check_available_ram() {
 
 # Function to check if a plan exists
 check_plan_exists() {
-  local name="$1"
-  local sql="SELECT name FROM plans WHERE name='$name';"
+  local id="$1"
+  local sql="SELECT id FROM plans WHERE id='$id';"
   local result=$(mysql --defaults-extra-file=$config_file -D "$mysql_database" -N -B -e "$sql")
   echo "$result"
 }
 
 # Check for command-line arguments
 if [ "$#" -ne 13 ]; then
-    echo "Usage: opencli $script_name old_plan_name new_plan_name description domains_limit websites_limit disk_limit inodes_limit db_limit cpu ram docker_image bandwidth storage_file"
+    echo "Usage: opencli $script_name plan_id new_plan_name description domains_limit websites_limit disk_limit inodes_limit db_limit cpu ram docker_image bandwidth storage_file"
     exit 1
 fi
 
 # Capture command-line arguments
-old_plan_name="$1"
+plan_id="$1"
 new_plan_name="$2"
 description="$3"
 domains_limit="$4"
@@ -140,41 +140,36 @@ else
 fi
 
 # Check if the old plan exists in the database
-existing_plan=$(check_plan_exists "$old_plan_name")
+existing_plan=$(check_plan_exists "$plan_id")
 if [ -z "$existing_plan" ]; then
-  echo "Old plan name '$old_plan_name' does not exist."
+  echo "Plan with id '$plan_id' does not exist."
   exit 1
 fi
 
 list_users () {
-local users="$1"
-plan_id="$2"
+  local plan_id="$1"
+  
+  # Construct SQL query to select plan name based on ID
+  local sql="SELECT name FROM plans WHERE id='$plan_id'"
+  
+  # Execute MySQL query
+  local result=$(mysql --defaults-extra-file="$config_file" -D "$mysql_database" -e "$sql")
+  
+  # Extract plan name from query result
+  local new_plan_name=$(echo "$result" | awk 'NR>1')
+  
 
-count=$(opencli plan-usage "$users" --json | grep -o '"username": "[^"]*' | sed 's/"username": "//' | wc -l)
+
+
+    count=$(opencli plan-usage "$new_plan_name" --json | grep -o '"username": "[^"]*' | sed 's/"username": "//' | wc -l)
 
     if [ "$count" -eq 0 ]; then
-        echo "Updated plan '$old_plan_name' to '$new_plan_name'"
-        # Perform action A
-    else
-        echo "successfully updated plan '$new_plan_name', you curently have $count users on the plan, to apply limits to all of them run comand: opencli plan-apply $plan_id --all"
-        # Perform action B
+        echo "Updated plan id $plan_id"
+    else    
+        echo "successfully updated plan id '$plan_id', you curently have $count users on the plan, to apply limits to all of them run comand: opencli plan-apply $plan_id --all"
     fi
 }
 
-
-
-get_plan_name() {
-    local name="$1"
-    local sql='SELECT id FROM plans WHERE name="$name";'
-    local result=$(mysql --defaults-extra-file=$config_file -D "$mysql_database" -N -B -e "$sql")
-    plan_id="$result"
-    echo $plan_id
-}
-
-get_plan_name "$new_plan_name"
-echo $plan_id
-
-
 # Call the update_plan function with the provided values
-update_plan "$old_plan_name" "$new_plan_name" "$description" "$domains_limit" "$websites_limit" "$disk_limit" "$inodes_limit" "$db_limit" "$cpu" "$ram" "$docker_image" "$bandwidth" "$storage_file"
-list_users "$new_plan_name" "$plan_id"
+update_plan "$plan_id" "$new_plan_name" "$description" "$domains_limit" "$websites_limit" "$disk_limit" "$inodes_limit" "$db_limit" "$cpu" "$ram" "$docker_image" "$bandwidth" "$storage_file"
+list_users "$plan_id"
