8c65a96b5537d6e27ca744576cdec0e6b2fa7446
Stefan Pejcic <stefan@pejcic.rs>
2024-05-04 14:02:30 +0200

    Update scheduler.sh

--- backup/scheduler.sh
+++ backup/scheduler.sh
@@ -29,8 +29,7 @@
 ################################################################################
 
 DEBUG=false
-
-# Directory containing JSON files
+run_id=""
 json_dir="/usr/local/admin/backups/jobs/"
 
 for arg in "$@"; do
@@ -38,64 +37,82 @@ for arg in "$@"; do
         --debug)
             DEBUG=true
             ;;
+        --run=*)
+            run_id="${arg#*=}"
+            ;;
     esac
 done
 
+# Function to convert schedule to cron format
+schedule_to_cron() {
+    case "$1" in
+        "hourly")   echo "0 * * * *";;
+        "daily")    echo "0 1 * * *";;
+        "weekly")   echo "0 1 * * SUN";;
+        "monthly")  echo "0 0 1 * *";;
+        *)          echo "ERROR: Invalid schedule value"; exit 1;;
+    esac
+}
 
-# remove previous backup schedules
-sed -i '/opencli backup-run/d' /etc/crontab
-
-# Loop through each JSON file in the directory
-for file in "$json_dir"*.json; do
-    # Check if the file is a regular file and has a status "on"
-    if [ -f "$file" ] && grep -q '"status": "on"' "$file"; then
-        # Extract destination, schedule, type, and filters values
-        destination=$(jq -r '.destination' "$file")
-        schedule=$(jq -r '.schedule' "$file")
-        type=$(jq -r '.type | .[]' "$file")
-        filters=$(jq -r '.filters | map(. + "") | join(" ")' "$file")
-
-        # Convert schedule to cron format
-        case "$schedule" in
-            "hourly")
-                cron_schedule="0 * * * *"
-                ;;
-            "daily")
-                cron_schedule="0 1 * * *"
-                ;;
-            "weekly")
-                cron_schedule="0 1 * * SUN"
-                ;;
-            "monthly")
-                cron_schedule="0 0 1 * *"
-                ;;
-            *)
-                echo "Invalid schedule value in $file"
-                continue
-                ;;
-        esac
-
-        # Determine flag based on type value
-        if [[ "$type" =~ "configuration" ]]; then
-            flag="--conf"
-        elif [[ "$type" =~ "accounts" ]]; then
-            # If there are filters, add flags for each filter; otherwise, add --all
-            if [ -n "$filters" ]; then
-                IFS=' ' read -ra filter_array <<< "$filters"
+# Function to determine flag based on type value
+determine_flag() {
+    local flags=""
+    case "$1" in
+        "configuration") flags="--conf";;
+        "accounts")
+            if [ -n "$2" ]; then
+                IFS=' ' read -ra filter_array <<< "$2"
                 for filter in "${filter_array[@]}"; do
-                    flag+=" --$filter"
+                    flags+=" --$filter"
                 done
             else
-                flag="--all"
+                flags="--all"
             fi
-        else
-            echo "Invalid type value in $file"
-            continue
-        fi
+            ;;
+        *)  echo "ERROR: Invalid type value"; exit 1;;
+    esac
+    echo "$flags"
+}
 
+# Main function to process backup jobs
+process_backup_job() {
+    local file="$1"
+    local schedule=$(jq -r '.schedule' "$file")
+    local destination=$(jq -r '.destination' "$file")
+    local type=$(jq -r '.type | .[]' "$file")
+    local filters=$(jq -r '.filters | map(. + "") | join(" ")' "$file")
+    local flag=$(determine_flag "$type" "$filters")
+
+    if [ -z "$run_id" ]; then
+        local cron_schedule=$(schedule_to_cron "$schedule")
         if [ "$DEBUG" = true ]; then
-            echo "$cron_schedule opencli backup-run $(basename "$file" .json) $flag"
+            printf "%s %s %s %s\n" "$cron_schedule" "opencli backup-run $(basename "$file" .json)" "$flag" >> /etc/crontab
+            printf "%s %s %s %s\n" "$cron_schedule" "opencli backup-run $(basename "$file" .json)" "$flag"
+        else
+            printf "%s %s %s %s\n" "$cron_schedule" "opencli backup-run $(basename "$file" .json)" "$flag" >> /etc/crontab
         fi
-            echo "$cron_schedule opencli backup-run $(basename "$file" .json) $flag" >> /etc/crontab
+    else
+        echo "opencli backup-run $(basename "$file" .json)" "$flag"
     fi
-done
+}
+
+
+# Remove previous backup schedules
+sed -i '/opencli backup-run/d' /etc/crontab
+
+# Process one job if run_id is provided, otherwise process and schedule all jobs
+if [ "$run_id" ]; then
+    file="/usr/local/admin/backups/jobs/$run_id.json"
+    if [ -f "$file" ]; then
+        process_backup_job "$file"
+    else
+        echo "ERROR: JSON file '$run_id.json' not found."
+        exit 1
+    fi
+else
+    for file in /usr/local/admin/backups/jobs/*.json; do
+        if [ -f "$file" ] && jq -e '.status == "on"' "$file" >/dev/null; then
+            process_backup_job "$file"
+        fi
+    done
+fi
