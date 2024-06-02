2b504251d9f00ea6782134c21b1c05e9a227397c
Stefan Pejcic <stefan@pejcic.rs>
2024-06-02 10:21:10 +0200

    Update add.sh

--- user/add.sh
+++ user/add.sh
@@ -6,7 +6,7 @@
 # Docs: https://docs.openpanel.co/docs/admin/scripts/users#add-user
 # Author: Stefan Pejcic
 # Created: 01.10.2023
-# Last Modified: 30.05.2024
+# Last Modified: 02.06.2024
 # Company: openpanel.co
 # Copyright (c) openpanel.co
 # 
