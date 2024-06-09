c1fda8a2875ced3f49c620b745bfb7a7b1f228f6
Stefan Pejcic <stefan@pejcic.rs>
2024-06-09 17:18:19 +0200

    Update install_php_version.sh

--- php/install_php_version.sh
+++ php/install_php_version.sh
@@ -5,7 +5,7 @@
 # Usage: opencli php-install_php_version <username> <php_version>
 # Author: Stefan Pejcic
 # Created: 07.10.2023
-# Last Modified: 07.12.2023
+# Last Modified: 09.06.2024
 # Company: openpanel.co
 # Copyright (c) openpanel.co
 # 
@@ -71,7 +71,62 @@ extensions_to_install=(
   php$php_version-opcache
   php$php_version-redis
   php$php_version-memcached
+  php$php_version-xdebug
+  php$php_version-apcu
+  php$php_version-imap
+  php$php_version-pgsql
+  php$php_version-odbc
+  php$php_version-dba
+  php$php_version-enchant
+  php$php_version-gmp
+  php$php_version-snmp
+  php$php_version-soap
+  php$php_version-pspell
+  php$php_version-recode
+  php$php_version-gettext
+  php$php_version-sybase
+  php$php_version-shmop
+  php$php_version-sysvmsg
+  php$php_version-sysvsem
+  php$php_version-sysvshm
+  php$php_version-tokenizer
+  php$php_version-wddx
+  php$php_version-xsl
+  php$php_version-interbase
+  php$php_version-mcrypt
+  php$php_version-mysqli
+  php$php_version-pdo
+  php$php_version-pdo_dblib
+  php$php_version-pdo_firebird
+  php$php_version-pdo_mysql
+  php$php_version-pdo_odbc
+  php$php_version-pdo_pgsql
+  php$php_version-pdo_sqlite
+  php$php_version-phalcon
+  php$php_version-radius
+  php$php_version-readline
+  php$php_version-reflection
+  php$php_version-session
+  php$php_version-simplexml
+  php$php_version-sodium
+  php$php_version-solr
+  php$php_version-sqlite3
+  php$php_version-stomp
+  php$php_version-sysvshm
+  php$php_version-tcpdf
+  php$php_version-tidy
+  php$php_version-uploadprogress
+  php$php_version-uuid
+  php$php_version-wddx
+  php$php_version-xcache
+  php$php_version-xdebug
+  php$php_version-xmlreader
+  php$php_version-xmlwriter
+  php$php_version-yaml
+  php$php_version-zip
+  php$php_version-zlib
 )
+
 
 
 echo "## Started installation for PHP version $php_version"
