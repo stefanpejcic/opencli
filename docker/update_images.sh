410c5033e90a28b0b1ab87ea277de6442d375f46
Stefan Pejcic <stefan@pejcic.rs>
2024-06-09 17:06:16 +0200

    Update update_images.sh

--- docker/update_images.sh
+++ docker/update_images.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 ################################################################################
 # Script Name: docker/update_images.sh
-# Description: Downloads docker images from hub.openpanel.co
+# Description: Downloads docker images from docker hub
 # Usage: opencli docker-update_images
 # Docs: https://docs.openpanel.co/docs/admin/scripts/docker#update-images
-# Author: Radovan Jecmenica
+# Author: Stefan Pejcic
 # Created: 30.11.2023
-# Last Modified: 09.05.2024
+# Last Modified: 09.06.2024
 # Company: openpanel.co
 # Copyright (c) openpanel.co
 # 
@@ -39,23 +39,19 @@ for arg in "$@"; do
     esac
 done
 
-# Check if OpenPanel Nginx image has updates
-nginx_update=$(docker pull openpanel/nginx 2>&1 | grep -i "Status: Image is up to date" | wc -l)
-
-# Check if OpenPanel Apache image has updates
-apache_update=$(docker pull openpanel/apache 2>&1 | grep -i "Status: Image is up to date" | wc -l)
-
-# If both images are already up to date, inform the user
-if [ "$nginx_update" -eq 1 ] && [ "$apache_update" -eq 1 ]; then
-    echo "OpenPanel Nginx and Apache images are already up to date."
-# If updates are available for either image, pull them
-else
-    if [ "$nginx_update" -ne 1 ]; then
-        echo "Newer OpenPanel Nginx image is available, updating.."
-        docker pull openpanel/nginx
+# Function to check for updates and pull the latest image if needed
+check_update() {
+    local image_name=$1
+    local update_status=$(docker pull "$image_name" 2>&1 | grep -i "Status: Image is up to date" | wc -l)
+    
+    if [ "$update_status" -ne 1 ]; then
+        echo "Newer $image_name image is available, updating.."
+        docker pull "$image_name"
+    else
+        echo "$image_name is already up to date."
     fi
-    if [ "$apache_update" -ne 1 ]; then
-        echo "Newer OpenPanel Apache image is available, updating.."
-        docker pull openpanel/apache
-    fi
-fi
+}
+
+# Check for updates on both Nginx and Apache images
+check_update "openpanel/nginx"
+check_update "openpanel/apache"
