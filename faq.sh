034b46464488cb56b1622a336d8bf201ccca3f62
Stefan Pejcic <stefan@pejcic.rs>
2024-06-11 19:18:38 +0200

    Update faq.sh

--- faq.sh
+++ faq.sh
@@ -33,7 +33,7 @@ BLUE='\033[0;34m'
 RED='\033[0;31m'
 PURPLE='\033[0;35m'
 NC='\033[0m' #reset
-CONFIG_FILE_PATH='/usr/local/panel/conf/panel.config'
+CONFIG_FILE_PATH='/etc/openpanel/openpanel/conf/openpanel.config'
 service_name="admin"
 
 read_config() {
