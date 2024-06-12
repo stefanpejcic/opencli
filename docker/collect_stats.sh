5434e87030124783e75d429b9a72f3d45a0efa2f
Stefan Pejcic <stefan@pejcic.rs>
2024-06-13 01:25:53 +0200

    Update collect_stats.sh

--- docker/collect_stats.sh
+++ docker/collect_stats.sh
@@ -30,8 +30,43 @@
 
 output_dir="/etc/openpanel/openpanel/core/stats"
 current_datetime=$(date +'%Y-%m-%d-%H-%M-%S')
+#!/bin/bash
+################################################################################
+# Script Name: collect_stats.sh
+# Description: Collect docker usage information for all users.
+# Usage: opencli docker-collect_stats
+# Author: Petar Curic, Stefan Pejcic
+# Created: 07.10.2023
+# Last Modified: 15.11.2023
+# Company: openpanel.co
+# Copyright (c) openpanel.co
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+################################################################################
 
-# Fix bug with high RAM usage reported for containers
+# Define the output directory
+output_dir="/usr/local/panel/core/stats"
+
+# Get the current date and time in the desired format
+current_datetime=$(date +'%Y-%m-%d-%H-%M-%S')
+
+# fix bug with high ram usage reported for containers
 sync
 echo 1 > /proc/sys/vm/drop_caches
 
@@ -51,36 +86,29 @@ ensure_jq_installed() {
 
 ensure_jq_installed
 
-# Function to process each container's stats
-process_container_stats() {
-    local container_stats="$1"
+# Loop through the Docker containers and extract data
+docker stats --no-stream --format '{{json .}}' | while read -r container_stats; do
+  # Extract relevant data from the JSON
+  cpu_percent=$(echo "$container_stats" | jq -r '.CPUPerc' | sed 's/%//')
+  mem_percent=$(echo "$container_stats" | jq -r '.MemPerc' | sed 's/%//')
+  net_io=$(echo "$container_stats" | jq -r '.NetIO' | awk '{print $1}' | sed 's/B//')
+  block_io=$(echo "$container_stats" | jq -r '.BlockIO' | awk '{print $1}' | sed 's/B//')
 
-    # Extract relevant data from the JSON
-    cpu_percent=$(echo "$container_stats" | jq -r '.CPUPerc' | sed 's/%//')
-    mem_percent=$(echo "$container_stats" | jq -r '.MemPerc' | sed 's/%//')
-    net_io=$(echo "$container_stats" | jq -r '.NetIO' | awk '{print $1}' | sed 's/B//')
-    block_io=$(echo "$container_stats" | jq -r '.BlockIO' | awk '{print $1}' | sed 's/B//')
+  # Extract the username (Name field from the Docker stats)
+  username=$(echo "$container_stats" | jq -r '.Name')
 
-    # Extract the username (Name field from the Docker stats)
-    username=$(echo "$container_stats" | jq -r '.Name')
+  # Define the output file path
+  output_file="$output_dir/$username/$current_datetime.json"
 
-    # Define the output file path
-    output_file="$output_dir/$username/$current_datetime.json"
+  # Create the directory if it doesn't exist
+  mkdir -p "$(dirname "$output_file")"
 
-    # Create the directory if it doesn't exist
-    mkdir -p "$(dirname "$output_file")"
+  # Create the JSON data and write it to the output file
+  json_data="{\"cpu_percent\": $cpu_percent, \"mem_percent\": $mem_percent, \"net_io\": \"$net_io\", \"block_io\": \"$block_io\"}"
+  # NOTE: net_io and block_io also contain the unit so should be used as strings.
+  echo "$json_data" > "$output_file"
 
-    # Create the JSON data and write it to the output file
-    json_data="{\"cpu_percent\": $cpu_percent, \"mem_percent\": $mem_percent, \"net_io\": \"$net_io\", \"block_io\": \"$block_io\"}"
-    # NOTE: net_io and block_io also contain the unit so should be used as strings.
-    echo "$json_data" > "$output_file"
+  echo "Data for $username written to $output_file"
 
-    echo "Data for $username written to $output_file"
-}
-
-export -f process_container_stats
-export output_dir
-export current_datetime
-
-# Loop through the Docker containers and extract data in parallel
-docker stats --no-stream --format '{{json .}}' | xargs -I {} -P 4 bash -c 'process_container_stats "$@"' _ {}
+  #echo "$json_data"
+done
