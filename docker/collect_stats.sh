2841276f968f2d6a0b07718c2b7b247c6c9f76be
Stefan Pejcic <stefan@pejcic.rs>
2024-06-25 18:49:11 +0200

    Update collect_stats.sh

--- docker/collect_stats.sh
+++ docker/collect_stats.sh
@@ -30,41 +30,6 @@
 
 output_dir="/etc/openpanel/openpanel/core/stats"
 current_datetime=$(date +'%Y-%m-%d-%H-%M-%S')
-#!/bin/bash
-################################################################################
-# Script Name: collect_stats.sh
-# Description: Collect docker usage information for all users.
-# Usage: opencli docker-collect_stats
-# Author: Petar Curic, Stefan Pejcic
-# Created: 07.10.2023
-# Last Modified: 15.11.2023
-# Company: openpanel.co
-# Copyright (c) openpanel.co
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-################################################################################
-
-# Define the output directory
-output_dir="/etc/openpanel/openpanel/core/stats"
-
-# Get the current date and time in the desired format
-current_datetime=$(date +'%Y-%m-%d-%H-%M-%S')
 
 # fix bug with high ram usage reported for containers
 sync
