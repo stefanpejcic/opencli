1454a426f4411f9be1cfa8adbcddd76f54c6cc4e
Stefan Pejcic <stefan@pejcic.rs>
2024-07-22 12:25:27 +0200

    Update sudo.sh

--- user/sudo.sh
+++ user/sudo.sh
@@ -35,37 +35,81 @@ if [ $# -lt 2 ] || [ $# -gt 3 ]; then
     exit 1
 fi
 
-username="$1"
+USERNAME="$1"
 action="$2"
 entrypoint_path="/etc/entrypoint.sh"
 
 # Check if the container exists
-container_id=$(docker ps -q -f name="$username")
+container_id=$(docker ps -q -f name="$USERNAME")
 if [ -z "$container_id" ]; then
-    echo "ERROR: Docker container for username '$username' not found."
+    echo "ERROR: Docker container for username '$USERNAME' not found."
     exit 1
 fi
 
 
-if [ "$action" == "enable" ]; then
-    docker exec "$container_id" sed -i "s/SUDO=\"[^\"]*\"/SUDO=\"YES\"/" "$entrypoint_path"
-    #docker exec "$container_id" usermod -aG sudo -u "$username"
-    echo "SUDO enabled for user $username."
-elif [ "$action" == "disable" ]; then
-    docker exec "$container_id" sed -i "s/SUDO=\"[^\"]*\"/SUDO=\"NO\"/" "$entrypoint_path"
-    docker exec "$container_id" sed -i "/^sudo:.*$username/d" /etc/group
-    echo "SUDO disabled for user $username."
-elif [ "$action" == "status" ]; then
-    status=$(docker exec "$container_id" grep -m 1 -o 'SUDO="[^"]*"' "$entrypoint_path" | cut -d'"' -f2)
-    if [ "$status" == "YES" ]; then
-        echo "SUDO is enabled for user ${username}."
-    elif [ "$status" == "NO" ]; then
-        echo "SUDO is disabled for user ${username}."
+set_root_user_passwd(){
+    USERNAME="$1"
+    password_hash=$(docker exec "$container_id" bash -c "getent shadow $USERNAME | cut -d: -f2")
+    if [ -z "$password_hash" ]; then
+        echo "ERROR: Failed to retrieve password hash for user $user_1000."
+        exit 1
     else
-        echo "Unknown status."
+        docker exec "$container_id" bash -c "sed -i 's/^root:[^:]*:/root:$password_hash:/' /etc/shadow"
+        
+        if [ $? -eq 0 ]; then
+            docker exec "$container_id" bash -c "sed -i 's/SUDO=\"[^\"]*\"/SUDO=\"YES\"/' \"$entrypoint_path\""
+            if [ $? -eq 0 ]; then
+                #echo "Successfully set the password for root user to match password of user $USERNAME."
+                echo "'su -' access enabled for user $USERNAME."
+            else 
+                echo "Failed to update root's password to match the user."
+            fi
+        else
+            echo "Failed to update root's password to match the user."
+        fi
+    fi
+}
+
+reset_root_user_password() {
+    strong_password=$(openssl rand -base64 32)
+    password_hash=$(docker exec "$container_id" bash -c "echo $strong_password | openssl passwd -1 -stdin")
+    docker exec "$container_id" bash -c 'sed -i "s/^root:[^:]*:/root:$password_hash:/" /etc/shadow'
+    if [ $? -eq 0 ]; then
+        #echo "User $ Successfully set a strong password for the root user."
+        docker exec "$container_id" sed -i "s/SUDO=\"[^\"]*\"/SUDO=\"NO\"/" "$entrypoint_path"
+        docker exec "$container_id" sed -i "/^sudo:.*$USERNAME/d" /etc/group 
+        echo "'su -' access disabled for user $USERNAME."
+    else
+        echo "ERROR: Failed to update root's password to a strong password."
+    fi
+}
+
+check_sudo_status(){
+
+        status=$(docker exec "$container_id" grep -m 1 -o 'SUDO="[^"]*"' "$entrypoint_path" | cut -d'"' -f2)
+        if [ "$status" == "YES" ]; then
+            echo "'su -' is enabled for user ${USERNAME}."
+        elif [ "$status" == "NO" ]; then
+            echo "'su -' is disabled for user ${USERNAME}."
+        else
+            echo "Unknown status."
+            exit 1
+        fi
+}
+
+
+manage_sudo_access() {
+    if [ "$action" == "enable" ]; then
+        set_root_user_passwd "$USERNAME"
+    elif [ "$action" == "disable" ]; then
+        reset_root_user_password
+    elif [ "$action" == "status" ]; then
+        check_sudo_status
+    else
+        echo "Invalid action. Please choose 'enable', 'disable', or 'status'."
         exit 1
     fi
-else
-    echo "Invalid action. Please choose 'enable', 'disable', or 'status'."
-    exit 1
-fi
+}
+
+
+manage_sudo_access
