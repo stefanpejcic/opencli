1705617982f3e770bd103a12ddb8be325779bc06
Stefan Pejcic <stefan@pejcic.rs>
2024-07-29 14:47:36 +0200

    Update user.sh

--- ssl/user.sh
+++ ssl/user.sh
@@ -2,7 +2,7 @@
 ################################################################################
 # Script Name: ssl/user.sh
 # Description: Check SSL status for all domains owned by user and update cache files.
-# Usage: opencli ssl-user <username|--all>
+# Usage: opencli ssl-user <username|--all> [--generate]
 # Author: Stefan Pejcic
 # Created: 22.11.2023
 # Last Modified: 28.07.2024
@@ -28,25 +28,32 @@
 # THE SOFTWARE.
 ################################################################################
 
-# Check if username is provided as an argument
-if [ $# -eq 0 ]; then
-    echo "Usage: opencli ssl-user <username> OR opencli ssl-user --all"
-  exit 1
-fi
 
-process_user_domains(){
-    # Set the username and file path
-    local username="$1"
-    file_path="/etc/openpanel/openpanel/core/users/$username/.ssl"
-    mkdir -p "$(dirname "$file_path")"
-    
-    # Get list of user domains
-    domains=$(opencli domains-user "$username")
-
+generate_ssl_for_each_domain(){
       # Check if no domains found
       if [[ -z "$domains" || "$domains" =~ ^No\ domains\ found\ for\ user ]]; then
         echo "No domains found in the database or opencli command error."
       else
+
+        for domain in $domains; do
+            echo "Processing domain: $domain"
+            # here check if already ahs paid and valid, then skip.
+            #ELSE DO
+            opencli ssl-domain $domain
+            #this auto triggers opencli ssl-user so it will add it to .ssl file
+        done
+      fi
+        
+}
+
+
+list_expiration_dates_for_ssl(){
+      # Check if no domains found
+      if [[ -z "$domains" || "$domains" =~ ^No\ domains\ found\ for\ user ]]; then
+        echo "No domains found in the database or opencli command error."
+      else
+
+        # TODO: add check for paid SSLs!
        
         # Get certificates information
         certificates_info=$(certbot certificates 2>&1)
@@ -70,9 +77,60 @@ process_user_domains(){
         
       fi
 }
+      
 
 
-if [[ "$1" == "--all" ]]; then
+process_user_domains(){
+    # Set the username and file path
+    local username="$1"
+    file_path="/etc/openpanel/openpanel/core/users/$username/.ssl"
+    mkdir -p "$(dirname "$file_path")"
+    
+    # Get list of user domains
+    domains=$(opencli domains-user "$username")
+
+      # Check if no domains found
+      if [[ -z "$domains" || "$domains" =~ ^No\ domains\ found\ for\ user ]]; then
+        echo "No domains found in the database or opencli command error."
+        exit 1
+    else
+    
+        if [[ "$2" == "--generate" ]]; then
+            # generate SSL for each domain available for the user
+            generate_ssl_for_each_domain
+        else    
+            #just check like we did so far..
+            list_expiration_dates_for_ssl
+        fi    
+        
+    fi
+}
+
+
+
+
+# Check if username is provided as an argument
+if [ $# -eq 0 ]; then
+    echo "Usage: opencli ssl-user <username> [--generate] OR opencli ssl-user --all [--generate]"
+    echo ""
+    echo "Example: Generate SSL for all domains owned by user 'stefan':"
+    echo "opencli ssl-user stefan --generate"
+    echo ""
+    echo "Example: Regenerate list of certificates for user 'stefan':"
+    echo "opencli ssl-user stefan"
+    echo ""
+    echo "Example: Generate SSL for all domains owned by all users:"
+    echo "opencli ssl-user --all --generate"
+    echo ""
+    echo ""
+    echo "Example: Regenerate list of certificates for all users:"
+    echo "opencli ssl-user --all"
+    echo ""
+    
+opencli ssl-user <username|--all> [--generate]
+    
+  exit 1
+elif [[ "$1" == "--all" ]]; then
   # Fetch list of users from opencli user-list --json
   users=$(opencli user-list --json | grep -v 'SUSPENDED' | awk -F'"' '/username/ {print $4}')
 
@@ -89,7 +147,12 @@ if [[ "$1" == "--all" ]]; then
   current_user_index=1
   for user in $users; do
     echo "Processing user: $user ($current_user_index/$total_users)"
-    process_user_domains "$user"
+    if [[ "$2" == "--generate" ]]; then
+        process_user_domains "$user" "--generate"
+    else
+        process_user_domains "$user"
+    fi
+    
     echo "------------------------------"
     ((current_user_index++))
   done
@@ -97,4 +160,8 @@ if [[ "$1" == "--all" ]]; then
   
 elif [ $# -eq 1 ]; then
   process_user_domains "$1"
+elif [[ $# -eq 2 ]] && [[ "$2" == "--generate" ]]; then
+
+  process_user_domains "$1" "--generate"
+ 
 fi
