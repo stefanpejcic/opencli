caf21617330131d63bf480e38fb99cdac6713ae2
Stefan Pejcic <stefan@pejcic.rs>
2024-08-22 08:49:26 +0200

    Update rename.sh

--- user/rename.sh
+++ user/rename.sh
@@ -115,12 +115,12 @@ fi
 
 
 
-########### KRAJ PROVERA, RA PROMENU
-# since overlay has no storage files, lets just do  > /dev/null 2>&1
+########### KDONE CHECKS, RUN THE REMOUNT
 umount /home/storage_file_$old_username > /dev/null 2>&1
 mv /home/storage_file_$old_username /home/storage_file_$new_username > /dev/null 2>&1
 mv /home/$old_username /home/$new_username > /dev/null 2>&1
 mount -o loop /home/storage_file_$new_username /home/$new_username > /dev/null 2>&1
+sed -i.bak "/\/home\/storage_file_$old_username \/home\/$old_username ext4 loop 0 0/c\/home\/storage_file_$new_username \/home\/$new_username ext4 loop 0 0" /etc/fstab
 
 
 # Check if the container exists
