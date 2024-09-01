d271087e09164e31b7737db84e5e1a3d8c799d91
Stefan Pejcic <stefan@pejcic.rs>
2024-09-01 12:24:12 +0200

    Update and rename ini.shTODO to ini.sh

--- php/ini.sh
+++ php/ini.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
+################################################################################
+# Script Name: php/ini.sh
+# Description: View or change php.ini values of any php version for a user.
+# Usage: opencli php-ini <username> <action> <setting> [value]
+# Author: Stefan Pejcic
+# Created: 07.10.2023
+# Last Modified: 01.09.2024
+# Company: openpanel.com
+# Copyright (c) openpanel.com
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+################################################################################
+
 
 # Check if username, action, setting, and value arguments are provided
 if [ $# -lt 3 ]; then
-    echo "Usage: $0 <username> <action> <setting> [value]"
+    echo "Usage: openli php-ini <username> <action> <setting> [value]"
     exit 1
 fi
 
