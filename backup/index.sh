631bfa7e91a5d169c39dd4630a2859bf5b4988cb
Stefan Pejcic <stefan@pejcic.rs>
2024-09-06 10:02:37 +0200

    Update index.sh

--- backup/index.sh
+++ backup/index.sh
@@ -39,6 +39,16 @@ fi
 DEBUG=false
 FORCE=false
 
+
+
+# IP SERVERS
+SCRIPT_PATH="/usr/local/admin/core/scripts/ip_servers.sh"
+if [ -f "$SCRIPT_PATH" ]; then
+    source "$SCRIPT_PATH"
+else
+    IP_SERVER_1=IP_SERVER_2=IP_SERVER_3="https://ip.openpanel.com"
+fi
+
 ensure_jq_installed() {
     # Check if jq is installed
     if ! command -v jq &> /dev/null; then
@@ -132,7 +142,7 @@ dest_destination_dir_name=$(echo "$dest_data" | awk 'NR==6')
 dest_storage_limit=$(echo "$dest_data" | awk 'NR==7')
 
 # Check if the destination hostname is local
-if [[ "$dest_hostname" == "localhost" || "$dest_hostname" == "127.0.0.1" || "$dest_hostname" == "$(curl -s https://ip.openpanel.co || wget -qO- https://ip.openpanel.co)" || "$dest_hostname" == "$(hostname)" ]]; then
+if [[ "$dest_hostname" == "localhost" || "$dest_hostname" == "127.0.0.1" || "$dest_hostname" == "$(curl --silent --max-time 2 -4 $IP_SERVER_1 || wget --timeout=2 -qO- $IP_SERVER_2 || curl --silent --max-time 2 -4 $IP_SERVER_3)" || "$dest_hostname" == "$(hostname)" ]]; then
     LOCAL=true
     REMOTE=false
 else
