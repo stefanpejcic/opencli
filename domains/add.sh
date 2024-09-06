73b08104b511264cc4af525ba885622c80d27263
Stefan Pejcic <stefan@pejcic.rs>
2024-09-06 10:01:36 +0200

    Update add.sh

--- domains/add.sh
+++ domains/add.sh
@@ -83,8 +83,15 @@ fi
 
 
 get_server_ipv4() {
-	# Get server ipv4 from ip.openpanel.co
-	current_ip=$(curl --silent --max-time 2 -4 https://ip.openpanel.co || wget --timeout=2 -qO- https://ip.openpanel.co || curl --silent --max-time 2 -4 https://ifconfig.me)
+	# IP SERVERS
+	SCRIPT_PATH="/usr/local/admin/core/scripts/ip_servers.sh"
+	if [ -f "$SCRIPT_PATH" ]; then
+	    source "$SCRIPT_PATH"
+	else
+	    IP_SERVER_1=IP_SERVER_2=IP_SERVER_3="https://ip.openpanel.com"
+	fi
+
+        current_ip=$(curl --silent --max-time 2 -4 $IP_SERVER_1 || wget --timeout=2 -qO- $IP_SERVER_2 || curl --silent --max-time 2 -4 $IP_SERVER_3)
 	# If site is not available, get the ipv4 from the hostname -I
 	if [ -z "$current_ip" ]; then
 	    current_ip=$(ip addr|grep 'inet '|grep global|head -n1|awk '{print $2}'|cut -f1 -d/)
