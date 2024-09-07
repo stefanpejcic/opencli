ab993aa309e0d16a8fd038153313c314bf8023ac
Stefan Pejcic <stefan@pejcic.rs>
2024-09-07 18:31:27 +0200

    Update setup.sh

--- email/setup.sh
+++ email/setup.sh
@@ -53,9 +53,46 @@ else
     exit 1
 fi
 
+# validate email
+is_valid_email() {
+  local email="$1"
+  # Regular expression for validating an email address
+  local email_regex='^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+  if [[ $email =~ $email_regex ]]; then
+    return 0  # Valid email
+  else
+    return 1  # Invalid email
+  fi
+}
 
-# Extract the command and arguments
+# REFRESH EMAILS LIST FOR USER!
+reload_emails_data_file_for_user() {
+    email="$1"
+    domain="${email#*@}"
+    whoowns_output=$(opencli domains-whoowns "$domain")
+    owner=$(echo "$whoowns_output" | awk -F "Owner of '$domain': " '{print $2}')
+    if [ -n "$owner" ]; then
+        file_to_refresh="/etc/openpanel/openpanel/core/users/$owner/emails.yml"
+        ALL_DOMAINS_OWNED_BY_USER=$(opencli domains-user $owner)
+        ALL_EMAILS_ON_SERVER=$(opencli email-setup email list)
+        
+        > "$file_to_refresh"
+        for domain in $ALL_DOMAINS_OWNED_BY_USER; do
+            echo "$ALL_EMAILS_ON_SERVER" | grep "@${domain}" >> "$file_to_refresh"
+        done
+    fi
+}
+
+
+
+
+# MAIN
 command="$@"
-
-# Execute the command inside the Docker container
 docker exec openadmin_mailserver setup $command
+
+
+if [[ "$1" == "email" && "$2" == "add" ]]; then
+  if is_valid_email "$3"; then
+    reload_emails_data_file_for_user $3
+  fi
+fi
