b6d8df0838ac0ac370b06c71e9cbec17eab79246
Stefan Pejcic <stefan@pejcic.rs>
2024-12-26 16:57:36 +0100

    Update change_plan.sh

--- user/change_plan.sh
+++ user/change_plan.sh
@@ -5,9 +5,9 @@
 # Usage: opencli user-change_plan <USERNAME> <NEW_PLAN_NAME>
 # Author: Petar Ćurić
 # Created: 17.11.2023
-# Last Modified: 30.05.2024
-# Company: openpanel.co
-# Copyright (c) openpanel.co
+# Last Modified: 26.12.2024
+# Company: openpanel.co,
+# Copyright (c) openpanel.co,
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -50,17 +50,32 @@ done
 # DB
 source /usr/local/admin/scripts/db.sh
 
+# COMPOSE 
+docker_compose_file="/etc/openpanel/docker/compose/$container_name/docker-compose.yml"
+if [[ ! -f "$docker_compose_file" ]]; then
+    echo "Fatal Error: $docker_compose_file does not exist - changing user limits will not be permanent." >&2
+    exit 1
+fi
+
 # Function to fetch the current plan ID for the container
 get_current_plan_id() {
     local container="$1"
-    local query="SELECT plan_id FROM users WHERE username = '$container'"
-    mysql --defaults-extra-file=$config_file -D "$mysql_database" -N -B -e "$query"
+    local query="SELECT plan_id, server FROM users WHERE username = '$container'"
+    local result
+    result=$(mysql --defaults-extra-file="$config_file" -D "$mysql_database" -N -B -e "$query")
+
+    # Extract plan_id and server from the result
+    current_plan_id=$(echo "$result" | awk '{print $1}')
+    server=$(echo "$result" | awk '{print $2}')
+    if [[ -z "$server" || "$server" == "default" ]]; then
+        server="$container"
+    fi
 }
 
 # Function to fetch plan limits for a given plan ID smece format
 get_plan_limits() {
     local plan_id="$1"
-    local query="SELECT cpu, ram, docker_image, storage_file, inodes_limit, bandwidth FROM plans WHERE id = '$plan_id'"
+    local query="SELECT cpu, ram, docker_image, disk_limit, inodes_limit, bandwidth FROM plans WHERE id = '$plan_id'"
     mysql --defaults-extra-file=$config_file -D "$mysql_database" -N -B -e "$query"
 }
 
@@ -90,7 +105,7 @@ get_plan_name() {
 }
 
 # Fetch current plan ID for the container
-current_plan_id=$(get_current_plan_id "$container_name")
+get_current_plan_id "$container_name"
 
 current_plan_name=$(get_plan_name "$current_plan_id")
 new_plan_id=$(get_new_plan_id "$new_plan_name")
@@ -104,8 +119,6 @@ fi
 # Fetch limits for the current plan
 current_plan_limits=$(get_plan_limits "$current_plan_id")
 
-##echo "Current plan limits:('$current_plan_limits')."
-
 # Check if the current plan limits were retrieved
 if [ -z "$current_plan_limits" ]; then
     echo "Error: Unable to fetch limits for the current plan ('$current_plan_id')."
@@ -114,7 +127,6 @@ fi
 
 # Fetch limits for the new plan
 new_plan_limits=$(get_plan_limits "$new_plan_id")
-##echo "New plan limits:('$new_plan_limits')."
 
 # Check if the new plan limits were retrieved
 if [ -z "$new_plan_limits" ]; then
@@ -124,228 +136,212 @@ fi
 
 
 
-#Limiti stari i novi cpu, ram, docker_image, storage_file, inodes_limit, bandwidth
-##echo "New plan ID:$new_plan_id"
-Ncpu=$(get_plan_limit "$new_plan_id" "cpu")
-Ocpu=$(get_plan_limit "$current_plan_id" "cpu")
-Nram=$(get_plan_limit "$new_plan_id" "ram")
-Oram=$(get_plan_limit "$current_plan_id" "ram")
-Ndocker_image=$(get_plan_limit "$new_plan_id" "docker_image")
-Odocker_image=$(get_plan_limit "$current_plan_id" "docker_image")
-Ndisk_limit=$(get_plan_limit "$new_plan_id" "storage_file")
-Odisk_limit=$(get_plan_limit "$current_plan_id" "storage_file")
-Ninodes_limit=$(get_plan_limit "$new_plan_id" "inodes_limit")
-Oinodes_limit=$(get_plan_limit "$current_plan_id" "inodes_limit")
-Nbandwidth=$(get_plan_limit "$new_plan_id" "bandwidth")
-Obandwidth=$(get_plan_limit "$current_plan_id" "bandwidth")
 
-#Serverski limiti za provere
-free_space=$(df -BG / | awk 'NR==2 {print $4}' | sed 's/G//')
+
+
+# LIMITS FROM DATABASE
+Ncpu=$(get_plan_limit "$new_plan_id" "cpu")
+Nram=$(get_plan_limit "$new_plan_id" "ram")
+numNram=$(echo "$Nram" | tr -d 'g')
+Ndocker_image=$(get_plan_limit "$new_plan_id" "docker_image")
+Ndisk_limit=$(get_plan_limit "$new_plan_id" "disk_usage")
+numNdisk=$(echo "$Ndisk_limit" | awk '{print $1}')
+Ninodes_limit=$(get_plan_limit "$new_plan_id" "inodes_limit")
+Nbandwidth=$(get_plan_limit "$new_plan_id" "bandwidth")
+storage_in_blocks=$((numNdisk * 1024000))
+
+
+
+
+
+
+# SERVER LIMITS
 maxCPU=$(nproc)
 maxRAM=$(free -g | awk '/^Mem/ {print $2}')
-numNram=$(echo "$Nram" | tr -d 'g')
-numOdisk=$(echo "$Odisk_limit" | awk '{print $1}')
-numNdisk=$(echo "$Ndisk_limit" | awk '{print $1}')
 
 
-if (($numNdisk>0)); then
-    nMounted=true
-else
-    nMounted=false
-fi
 
-oMounted=true
-if df -BG | grep -q "/home/$container_name"; then
-    # Directory is mounted
-    curSize=$(df -BG | grep "/home/$container_name" | awk 'NR==1 {print $3}' | sed 's/G//')
-        if $debug; then
-            echo "storage file IS currently mounted, current size: ${curSize}G"
-        fi
-else
-    # Directory is not mounted
-    curSize=$(du -sBG "/home/$container_name" | awk '{print $1}' | sed 's/G//')
-        if $debug; then
-            echo "storage file IS NOT currently mounted, current size: ${curSize}G" 
-        fi
-    oMounted=false
-fi
 
-addSize=0
-addInodes=0
-if [ "$oMounted" = "true" ] && [ "$nMounted" = "true" ]; then    
-    if $debug; then 
-        echo "BOTH MOUNTED"
+
+# counters
+success_count=0
+failure_count=0
+write_failure_count=0
+
+
+
+get_compose_limit() {
+    local key="$1"
+    local value=$(grep "^$key=" "$docker_compose_file" | cut -d'=' -f2)
+    if [[ -n $value ]]; then
+        echo "$value"
+    else
+        echo "Warning: Key '$key' not found in $docker_compose_file."
+        # fallback to mysql!
+        Ocpu=$(get_plan_limit "$current_plan_id" "$key")
+        Oram=$(get_compose_limit "$current_plan_id" "ram")
     fi
-    addSize=$((numNdisk - numOdisk))
-    addInodes=$((Ninodes_limit - Oinodes_limit))
-fi
-
-if $debug; then 
-    echo "New plan Disk limit - Old plan Disk limit = $addSize (set to 0 if new or old plan is unlimited)" 
-fi
-
-curInode=$(find /home/$container_name/. | wc -l)
-
-if (( $numNram > $maxRAM )); then
-    echo "Error: New RAM value exceeds the server limit, not enough physical memory - $numNram > $maxRam."
-    exit 1
-fi
-
-if (( $Ncpu > $maxCPU )); then
-    echo "Error: New CPU value exceeds the server limit, not enough CPU cores - $Ncpu > $maxCPU."
-    exit 1
-fi
-
-noDiskSpace=false
-if (( $numNdisk > $free_space )); then
-    echo "Error: Insufficient space on disk, no changes to disk limit were made, Available: ${free_space}GB - Required: $Ndisk_limit."
-    noDiskSpace=true
-fi
-
-if [[ "$Ndocker_image" != "$Odocker_image" ]]; then
-    echo "Error: Can't change docker image."
-    exit 1
-fi
-
-if (( $curSize > $numNdisk && $numNdisk!=0 )); then
-    echo "Error: Current size on disk exceeds the limits of the new plan - $curSize > $numNdisk."
-    exit 1
-fi
-
-if (( $curInode > $Ninodes_limit )); then
-    echo "Error: Current inode usage exceeds the limits of the new plan - $curInode > $Ninodes_limit."
-    exit 1
-fi
-
-#if (( $addInodes < 0 && $numNdisk!=0 )); then
-#    echo "Error: Storage downgrades are not possible."
-#    exit 1
-#fi
-
-if $debug; then
-    echo "          cpu, ram, docker_image, disk_limit, inodes_limit, bandwidth"
-    echo "Old plan: $Ocpu , $Oram, $Odocker_image, $Odisk_limit, $Oinodes_limit, $Obandwith"
-    echo "New plan: $Ncpu , $Nram, $Ndocker_image, $Ndisk_limit, $Ninodes_limit, $Nbandwith"
-
-    echo "Difference in cpu: $Ocpu to $Ncpu"
-fi
-
-docker update --cpus="$Ncpu" "$container_name" > /dev/null
-
-if $debug; then
-    echo "Difference in ram: $Oram to $Nram"
-fi
-
-docker update --memory="$Nram" --memory-swap="$Nram" "$container_name" > /dev/null
-
-if $debug; then
-    echo "Current docker image $Odocker_image"
-    echo "New docker image $Ndocker_image (must be the same for the plan change to work!)"
-    #ako je drugi image nema izmene plana
-
-    echo "Difference in disk_limit: $Odisk_limit to $Ndisk_limit"
-    echo "Difference in inodes_limit: $Oinodes_limit to $Ninodes_limit"
-    echo "New limits must be larger than or equal to old limits."
-fi
-
-if [ "$oMounted" = "true" ] && [ "$nMounted" = "true" ]; then
 
 
-    if (( $addSize > 0 || $addInodes > 0 )) && [ "$noDiskSpace" = false ]; then
-        docker stop $container_name
-        if mount | grep "/home/$container_name" > /dev/null; then
-            umount /home/$container_name
-        fi
 
-        #echo "falokejt parametar ${numNdisk}g"
-        fallocate -l ${numNdisk}g /home/storage_file_$container_name
 
-        #fix+resize FSystem
-        e2fsck -f -y /home/storage_file_$container_name  > /dev/null
-        resize2fs /home/storage_file_$container_name
-        mount -o loop /home/storage_file_$container_name /home/$container_name
-        docker start $container_name
-        if $debug; then
-            echo "Disk limit changed from ${numOdisk}G to ${numNdisk}G"
-        fi
 
-        if (( $addInodes > 0 )); then
-            #mkfs.ext4 -N $Ninodes_limit /home/storage_file_$container_name
-            echo "Warning: Increasing Inode limit is not possible, old plan limit remains"
-        fi
+# OLD LIMITS
+Odocker_image=$(get_plan_limit "$current_plan_id" "docker_image")
+Ocpu=$(get_compose_limit "cpu") # from compose file, fallback to mysql!
+Oram=$(get_compose_limit "ram") # from compose file, fallback to mysql!
 
-    elif (($addSize < 0)); then
-        echo "Warning: No change was made to the disk limit, new plan limit is more restrictive which is not allowed"
+
+
+
+
+# FUNCTIONS
+update_compose_value() {
+    local key="$1"
+    local value="$2"
+
+    sed -i "s/^$key=[^ ]*/$key=$value/" "$docker_compose_file" > /dev/null 2>&1
+    if grep -q "^$key=$value" "$docker_compose_file"; then
+        echo "Successfully updated $key to $value in $docker_compose_file."
+    else
+        ((write_failure_count++))
+        echo "[✘] Error changing $key value in docker compose file for the user"
+    fi
+}
+
+
+
+update_container_cpu() {
+    if (( $Ncpu > $maxCPU )); then
+        echo "Error: New CPU value exceeds the server limit, not enough CPU cores - $Ncpu > $maxCPU."
+        exit 1
+    
     else
         if $debug; then
-            echo "No change was made to the disk limit, new plan limit is the same as old limit."
+            echo "Updating CPU% limit from: $Ocpu to $Ncpu"
+        fi
+        command="docker --context $server update --cpus=\"$Ncpu\" \"$container_name\""
+        eval $command > /dev/null
+        if [ $? -eq 0 ]; then
+            ((success_count++))
+            echo "[✔] CPU limit ($Ncpu) changed successfully for container."
+        else
+            ((failure_count++))
+            echo "[✘] Error setting CPU limit for the container:"
+            echo "Command used: $command"
+        fi
+        update_compose_value "$docker_compose_file" "cpu" "$Ncpu"
+    fi
+}
+
+
+
+update_container_ram() {
+    if (( $numNram > $maxRAM )); then
+        echo "Warning: Ram limit not changed for the contianer -new value exceeds the server limit, not enough physical memory - $numNram > $maxRam."
+    else
+        if $debug; then
+            echo "Updating Memory limit from: $Oram to $Nram"
+        fi
+        command="docker --context $server update --memory=\"$Nram\" --memory-swap=\"$Nram\" \"$container_name\""
+        eval $command > /dev/null
+        if [ $? -eq 0 ]; then
+            ((success_count++))
+            echo "[✔] Memory limit $Nram changed successfully for container"
+        else
+            ((failure_count++))
+            echo "[✘] Error setting RAM limit for the container:"
+            echo "Command used: $command"
+        fi 
+        update_compose_value "$docker_compose_file" "memory" "$Nram"
+    fi
+}
+
+
+
+update_container_image() {
+    if [[ "$Ndocker_image" != "$Odocker_image" ]]; then
+        echo "[✘] Error: can't change docker image."
+        ((failure_count++))
+    else
+        if $debug; then
+            echo "[✔] Same docker image $Odocker_image is used on both plans."
+        fi
+        ((success_count++))
+    fi
+}
+
+
+
+update_used_disk_inodes() {
+    if $debug; then
+        echo "Changing disk limit from: $Odisk_limit to $Ndisk_limit ($storage_in_blocks)"
+        echo "Changing inodes limit from: $Oinodes_limit to $Ninodes_limit"
+    fi
+    command="setquota -u $container_name $storage_in_blocks $storage_in_blocks $Ninodes_limit $Ninodes_limit /"
+    eval $command # set quota for user
+    if [ $? -eq 0 ]; then
+        ((success_count++))
+        echo "[✔] Disk usage limit: $Ndisk_limit and inodes limit: $Ninodes_limit applied successfully to the user."
+    else
+        ((failure_count++))
+        echo "[✘] Error setting disk and inodes limits for the user:"
+        echo "Command used: $command"
+    fi 
+    quotacheck -avm > /dev/null                                                              # recheck for all users
+    repquota -u / > /etc/openpanel/openpanel/core/users/repquota                             # store to file for openpanel ui
+}
+
+
+
+# TODO
+update_user_tc() {
+    echo "Changing port speed to $Nbandwidth is not possible at the moment."
+    #((failure_count++))
+}
+
+
+change_plan_name_in_db() {
+    if $debug; then
+        echo "Changing plan name for user from '$current_plan_name' to: '$new_plan_name'"
+    fi
+    
+    #Menja ID
+    query="UPDATE users SET plan_id = $new_plan_id WHERE username = '$container_name';"
+    result=$(mysql -D "$mysql_database" -N -B -e "$query")
+    if [ $? -eq 0 ]; then
+        if [ $failure_count -gt 0 ]; then
+            echo "Plan changed successfuly for user $container_name from $current_plan_name to $new_plan_name - ($failure_count warnings)"
+        else
+            echo "Plan changed successfuly for user $container_name from $current_plan_name to $new_plan_name"
+        fi
+    else
+        echo "Error changing plan id in the database for user - is mysql service running?"
+    fi
+}
+
+tada() {
+    if [ $write_failure_count -gt 0 ]; then
+        echo ""
+        echo "Error changing $write_failure_count values in file: $docker_compose_file"
+        if $debug; then
+            echo "Current values:"
+            cat $docker_compose_file
         fi
     fi
-
-elif [ "$oMounted" = "true" ] && [ "$nMounted" != "true" ]; then
-
-    echo "Warning: disk size limit can't be set to unlimited after filesystem creation, original plan limit remains."
-        #umount /home/$container_name
-        #rm /home/storage_file_$container_name
-
-elif [ "$oMounted" != "true" ] && [ "$nMounted" = "true" ]; then
-    echo "Warning: disk usage cannot be limited after user was created on an unlimited plan, disk usage remains unlimited."
-    #if [ "$free_space" -le "$numNdisk" ]; then
-    #    echo "Error: Not enough free space on disk for storage file creation, no limit enforced on container, switch to a smaller plan or free up disk space."
-    #else
-        #docker stop $container_name
-        #fallocate -l ${numNdisk}g /home/storage_file_$container_name 
-        #mkfs.ext4 -N $Ninodes_limit /home/storage_file_$container_name 
-        #mount -o loop /home/storage_file_$container_name /home/$container_name
-        #docker start $container_name
-        #if $debug; then
-        #    echo "Container disk usage now limited to ${numNdisk}G"
-        #fi
-    #fi
-else
-    if $debug; then
-        echo "No change in disk, both new and original plan are unlimited."
-    fi
-fi
-##echo "Difference in bandwidth: $Obandwidth to $Nbandwidth"
-
-
-# Remove the current Docker network from the container
-#docker network disconnect "$current_plan_name" "$container_name"
-
-#novi catch all networks
-NETWORKS=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.NetworkID}} {{end}}' "$container_name")
-
-# Loop through each network and disconnect the container
-for network in $NETWORKS; do
-  docker network disconnect "$network" "$container_name"
-done
-
-if $debug; then
-    echo "old plan name: ('$current_plan_name')"
-fi
-# Connect the container to the new Docker network
-docker network connect "$new_plan_name" "$container_name"
-if $debug; then
-    echo "new plan name:('$new_plan_name')"
-fi
-#Menja ID
-query="UPDATE users SET plan_id = $new_plan_id WHERE username = '$container_name';"
-result=$(mysql -D "$mysql_database" -N -B -e "$query")
-if [ $? -eq 0 ]; then
-    echo "Plan changed successfuly for user $container_name from $current_plan_name to $new_plan_name"
-else
-    echo "Error when changing plan id in the database."
-fi
-
-#skripta za rewrite nginx vhosts za tog usera!
-if $debug; then
-    opencli nginx-update_vhosts $container_name --nginx-reload
-else
-    opencli nginx-update_vhosts $container_name --nginx-reload > /dev/null
-fi
-# Compare limits and list the differences
-#diff_output=$(diff -u <(echo "$current_plan_limits") <(echo "$new_plan_limits"))
+}
 
 
 
+
+
+# MAIN
+update_container_image          # todo
+update_container_cpu            # update cpu% on container and change in docker compose file
+update_container_ram            # update ram on container and change in docker compose file
+#update_user_tc                 # todo
+update_used_disk_inodes         # change quota for user, check all user files and update cached file for user panel
+change_plan_name_in_db          # finally store new plan for user in database
+tada                            # check if any errors writing data to compose file, if so, cpu and ram changes are not permanent!
+
+exit 0
