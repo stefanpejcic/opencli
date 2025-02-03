706439e34de65201a968b52c4a01516e788e9e89
Stefan Pejcic <stefan@pejcic.rs>
2025-02-03 15:50:13 +0100

    Update add.sh

--- domains/add.sh
+++ domains/add.sh
@@ -5,7 +5,7 @@
 # Usage: opencli domains-add <DOMAIN_NAME> <USERNAME> [--docroot DOCUMENT_ROOT] --debug
 # Author: Stefan Pejcic
 # Created: 20.08.2024
-# Last Modified: 21.01.2025
+# Last Modified: 03.02.2025
 # Company: openpanel.com
 # Copyright (c) openpanel.com
 # 
