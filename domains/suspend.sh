630fccb994dcbbb648fb8325bd36dcbaa9982e9a
Stefan Pejcic <stefan@pejcic.rs>
2025-02-05 23:49:27 +0100

    Update suspend.sh

--- domains/suspend.sh
+++ domains/suspend.sh
@@ -5,7 +5,7 @@
 # Usage: opencli domains-suspend <DOMAIN-NAME>
 # Author: Stefan Pejcic
 # Created: 04.11.2024
-# Last Modified: 04.12.2024
+# Last Modified: 05.02.2025
 # Company: openpanel.com
 # Copyright (c) openpanel.com
 # 
@@ -28,107 +28,49 @@
 # THE SOFTWARE.
 ################################################################################
 
-# DB
-source /usr/local/admin/scripts/db.sh
 
-
-
-# Function to fetch the owner username of a domain
-get_docker_context_for_user() {
-    local domain="$1"
-    
-    # Check if the config file exists
-    if [ ! -f "$config_file" ]; then
-        echo "Config file $config_file not found."
-        exit 1
-    fi
-    
-    # Query to fetch the user_id for the specified domain
-    user_id_query="SELECT user_id FROM domains WHERE domain_name = '$domain'"
-    
-    # Execute the query and fetch the user_id
-    user_id=$(mysql --defaults-extra-file="$config_file" -D "$mysql_database" -e "$user_id_query" -sN)
-
-    if [ -z "$user_id" ]; then
-        echo "Domain '$domain' not found in the database."
-        exit 1
+check_and_add_to_enabled() {
+    if docker exec caddy caddy validate --config /etc/caddy/Caddyfile >/dev/null 2>&1; then
+        docker exec caddy caddy reload --config /etc/caddy/Caddyfile >/dev/null 2>&1
+        return 0
     else
-        username_query="SELECT server FROM users WHERE id = '$user_id'"
-        server_name=$(mysql --defaults-extra-file="$config_file" -D "$mysql_database" -e "$username_query" -sN)
-        if [ -z "$server_name" ]; then
-            server_name="default" # compatibility with older panel versions before clustering
-            context_flag=""
-            node_ip_address=""
-        elif [ "$server_name" == "default" ]; then
-            context_flag=""
-            node_ip_address=""
-        else
-            context_flag="--context $server_name"
-            # GET IPV4 FOR SSH COMMANDS
-            context_info=$(docker context ls --format '{{.Name}} {{.DockerEndpoint}}' | grep "$server_name")
-        
-            if [ -n "$context_info" ]; then
-                endpoint=$(echo "$context_info" | awk '{print $2}')
-                if [[ "$endpoint" == ssh://* ]]; then
-                    node_ip_address=$(echo "$endpoint" | cut -d'@' -f2 | cut -d':' -f1)
-                else
-                    echo "ERROR: valid IPv4 address for context $server_name not found!"
-                    echo "       User container is located on node $server_name and there is a docker context with the same name but it has no valid IPv4 in the endpoint."
-                    echo "       Make sure that the docker context named $server_name has valid IPv4 address in format: 'SERVER ssh://USERNAME@IPV4' and that you can establish ssh connection using those credentials."
-                    exit 1
-                fi
-            else
-                echo "ERROR: docker context with name $server_name does not exist!"
-                echo "       User container is located on node $server_name but there is no docker context with that name."
-                echo "       Make sure that the docker context exists and is available via 'docker context ls' command."
-                exit 1
-            fi    
-        fi
+        return 1
     fi
 }
 
+validate_conf() {
+
+  	if [ $(docker ps -q -f name=caddy) ]; then
+        :
+	else
+        cd /root && docker compose up -d caddy  >/dev/null 2>&1
+     fi
+     
+	check_and_add_to_enabled
+ 
+	if [ $? -eq 0 ]; then
+		echo "Domain suspended successfully."
+	else
+        mv ${suspended_dir}${domain_name}.conf  $domain_vhost > /dev/null 2>&1
+        check_and_add_to_enabled
+		echo "ERROR: Failed to validate conf after suspend, changes reverted."
+	fi
+}
 
 edit_nginx_vhosts() {
-       domain_vhost="/etc/nginx/sites-available/$domain_name.conf"
+       domain_vhost="/etc/openpanel/caddy/domains/$domain_name.conf"
+       suspended_dir="/etc/openpanel/caddy/suspended_domains/"
+       conf_template="/etc/openpanel/caddy/templates/suspended.conf"
+       mkdir -p $suspended_dir
        if [ -f "$domain_vhost" ]; then
-            if [ "$DEBUG" = true ]; then
-              echo "Suspending domain: $domain_name"
-            fi
-            
-            if [ -n "$node_ip_address" ]; then
-                # TODO: INSTEAD OF ROOT USER SSH CONFIG OR OUR CUSTOM USER!
-                if [ "$DEBUG" = true ]; then
-                    ssh "root@$node_ip_address" "sed -i 's/set \$suspended_website 0;/set \$suspended_website 1;/g' $domain_vhost"
-                    sed_status=$?
-                else
-                    ssh "root@$node_ip_address" "sed -i 's/set \$suspended_website 0;/set \$suspended_website 1;/g' $domain_vhost" > /dev/null 2>&1
-                    sed_status=$?
-                fi
-            else
-                if [ "$DEBUG" = true ]; then
-                    sed -i 's/set $suspended_website 0;/set $suspended_website 1;/g' $domain_vhost
-                    sed_status=$?
-                else
-                    sed -i 's/set $suspended_website 0;/set $suspended_website 1;/g' $domain_vhost > /dev/null 2>&1
-                    sed_status=$?
-                fi
-            fi
-            
-            if [ "$DEBUG" = true ]; then
-                docker $context_flag exec nginx sh -c 'nginx -t && nginx -s reload'
-            else
-                docker $context_flag exec nginx sh -c 'nginx -t && nginx -s reload' > /dev/null 2>&1
-            fi
-
-            if [ $sed_status -eq 0 ]; then
-                echo "Domain $domain_name suspended successfully."
-            else
-                echo "ERROR: Failed to suspend domain $domain_name."
-                exit 1
-            fi
-            
+                       
+            cp $domain_vhost $suspended_dir  > /dev/null 2>&1
+        	domain_conf=$(cat "$conf_template" | sed -e "s|<DOMAIN_NAME>|$domain_name|g")
+            echo "$domain_conf" > "$domain_vhost"
+            validate_conf
+         
         else
-            echo "WARNING: vhost file for domain $domain_name does not exist"
+            echo "ERROR: vhost file for domain $domain_name does not exist"
         fi
 }
 
@@ -142,6 +84,4 @@ fi
 
 # Get the domain name from the command line argument
 domain_name="$1"
-
-get_docker_context_for_user "$domain_name"           # get node and ip
-edit_nginx_vhosts                                    # redirect domain to suspended_website.html
+edit_nginx_vhosts
