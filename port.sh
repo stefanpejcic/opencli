56fa25d428645da0ef83aefe6e7ea24ebdd25169
Stefan Pejcic <stefan@pejcic.rs>
2025-02-17 14:02:37 +0100

    Update port.sh

--- port.sh
+++ port.sh
@@ -65,7 +65,8 @@ update_env() {
 do_reload() {
   if [[ "$3" != '--no-restart' ]]; then
     cd $COMPOSE_DIR
-    nohup docker compose restart caddy openpanel > /dev/null 2>&1 &
+    nohup docker compose restart caddy > /dev/null 2>&1 &
+    nohup docker compose down openpanel && docker compose up -d openpanel > /dev/null 2>&1 &
    fi
 }
 
