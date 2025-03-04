1bf8c8b39049829d2c1c869447b1a54d3ff37db3
Stefan Pejcic <stefan@pejcic.rs>
2025-03-04 07:22:58 +0100

    Update add.sh

--- user/add.sh
+++ user/add.sh
@@ -1232,6 +1232,7 @@ fi
 
 postgres_password=$(openssl rand -base64 12)
 mysql_root_password=$(openssl rand -base64 12)
+pg_admin_password=$(openssl rand -base64 12)
 
 : '
     log "Using:"
@@ -1271,6 +1272,9 @@ sed -i -e "s|USERNAME=\"[^\"]*\"|USERNAME=\"$username\"|g" \
     -e "s|SSH_PORT=\"[^\"]*\"|SSH_PORT=\"127.0.0.1:$port_1\"|g" \
     -e "s|TTYD_PORT=\"[^\"]*\"|TTYD_PORT=\"$port_3\"|g" \
     -e "s|PMA_PORT=\"[^\"]*\"|PMA_PORT=\"$port_4\"|g" \
+    -e "s|POSTGRES_PASSWORD=\"[^\"]*\"|POSTGRES_PASSWORD=\"$postgres_password\"|g" \
+    -e "s|PGADMIN_PW=\"[^\"]*\"|PGADMIN_PW=\"$pg_admin_password\"|g" \
+    -e "s|PGADMIN_MAIL=\"[^\"]*\"|PGADMIN_EMAIL=\"$email\"|g" \
     -e "s|MYSQL_PORT=\"[^\"]*\"|MYSQL_PORT=\"127.0.0.1:$port_2\"|g" \
     -e "s|DEFAULT_PHP_VERSION=\"[^\"]*\"|DEFAULT_PHP_VERSION=\"$default_php_version\"|g" \
     -e "s|MYSQL_ROOT_PASSWORD=\"[^\"]*\"|MYSQL_ROOT_PASSWORD=\"$mysql_root_password\"|g" \
