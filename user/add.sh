81bc280ad71e0d7a6563b32ef94d29221687fca2
Stefan Pejcic <stefan@pejcic.rs>
2025-03-04 07:25:07 +0100

    Update add.sh

--- user/add.sh
+++ user/add.sh
@@ -1274,7 +1274,7 @@ sed -i -e "s|USERNAME=\"[^\"]*\"|USERNAME=\"$username\"|g" \
     -e "s|PMA_PORT=\"[^\"]*\"|PMA_PORT=\"$port_4\"|g" \
     -e "s|POSTGRES_PASSWORD=\"[^\"]*\"|POSTGRES_PASSWORD=\"$postgres_password\"|g" \
     -e "s|PGADMIN_PW=\"[^\"]*\"|PGADMIN_PW=\"$pg_admin_password\"|g" \
-    -e "s|PGADMIN_MAIL=\"[^\"]*\"|PGADMIN_EMAIL=\"$email\"|g" \
+    -e "s|PGADMIN_MAIL=\"[^\"]*\"|PGADMIN_MAIL=\"$email\"|g" \
     -e "s|MYSQL_PORT=\"[^\"]*\"|MYSQL_PORT=\"127.0.0.1:$port_2\"|g" \
     -e "s|DEFAULT_PHP_VERSION=\"[^\"]*\"|DEFAULT_PHP_VERSION=\"$default_php_version\"|g" \
     -e "s|MYSQL_ROOT_PASSWORD=\"[^\"]*\"|MYSQL_ROOT_PASSWORD=\"$mysql_root_password\"|g" \
