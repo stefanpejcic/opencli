e706eb14a447a4bbd9ab6fdaa7ab205e6e5f2bc8
Stefan Pejcic <stefan@pejcic.rs>
2025-04-10 15:13:09 +0200

    Update users.sh

--- ftp/users.sh
+++ ftp/users.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
-# Gather the contents of all users.list files into USER_FILES
-USER_FILES=$(cat /etc/openpanel/ftp/users/*/users.list)
-
-# Prepare the USERS variable
 USERS=""
-while IFS= read -r line; do
-  USERS="$USERS $line"
-done <<EOF
-$USER_FILES
-EOF
 
-# Remove any leading/trailing spaces from USERS
-USERS=$(echo "$USERS" | xargs)
+for dir in /etc/openpanel/ftp/users/*; do
+    file="$dir/users.list"
+    user=$(basename "$dir")
+    if [[ -f "$file" ]]; then
+        # Get the group ID of the user
+        group_id=$(id -g "$user")
+        
+        # Replace /var/www/html/ with /home/user/ and append group_id with '|' separator
+        while IFS= read -r line; do
+            modified_line=$(echo "$line" | sed "s|/var/www/html/|/home/${user}/docker-data/volumes/${user}_html_data/_data/|g")
+            USERS+="$modified_line|$group_id "
+        done < "$file"
+    fi
+done
 
-# Write USERS to the all.users file
+echo "$USERS" 
+
 echo "USERS=\"$USERS\"" > /etc/openpanel/ftp/all.users
