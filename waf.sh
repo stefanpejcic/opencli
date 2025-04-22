8ca78d7e8f5fda7e2db71a403f8a6183cc5e42c7
Stefan Pejcic <stefan@pejcic.rs>
2025-04-22 14:55:16 +0200

    Create waf.sh

---
+++ waf.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+################################################################################
+# Script Name: waf.sh
+# Description: Manage CorazaWAF
+# Usage: opencli waf <setting> 
+# Author: Stefan Pejcic
+# Created: 22.05.2025
+# Last Modified: 22.05.2025
+# Company: openpanel.com
+# Copyright (c) openpanel.com
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+################################################################################
+
+
+# Display usage information
+usage() {
+    echo "Usage: opencli waf <command> [options]"
+    echo ""
+    echo "Commands:"
+    echo "  status                                        Check if CorazaWAF is enabled for new domains and users."
+    echo ""
+    echo "Examples:"
+    echo "  opencli waf status"
+    exit 1
+}
+
+
+check_coraza_status() {
+  local env_file="/root/.env"
+  local custom_image='CADDY_IMAGE="openpanel/caddy-coraza"'
+  
+  if grep -q "^$custom_image" "$env_file"; then
+      echo "CorazaWAF is ENABLED"
+  else
+       echo "CorazaWAF is DISABLED"
+  fi
+}
+
+
+
+
+
+# MAIN
+# MAIN
+case "$1" in
+    "status")
+        check_coraza_status
+        ;;
+    "enable")
+        enable_coraza_waf
+        ;;
+    "disable")
+        disable_coraza_waf
+        ;;
+    "help")
+        usage
+        ;;
+    *)
+        echo "Invalid option: $1"
+        usage
+        exit 1
+        ;;
+esac
+
+exit 0
