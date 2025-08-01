a5bad7f194761be5a1ba436bca6233a7ecc5db14
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-08-01 10:51:17 +0200

    Update manage.sh

--- email/manage.sh
+++ email/manage.sh
@@ -39,7 +39,7 @@ fi
 
 
 # added in 0.2.5
-ENTERPRISE="/usr/local/admin/core/scripts/enterprise.sh"
+ENTERPRISE="/usr/local/opencli/enterprise.sh"
 PANEL_CONFIG_FILE="/etc/openpanel/openpanel/conf/openpanel.config"
 key_value=$(grep "^key=" $PANEL_CONFIG_FILE | cut -d'=' -f2-)
 
