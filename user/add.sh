e125d2be614bcc02e14f61a7865534d1a876d06e
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-11-03 13:14:24 +0100

    Update add.sh

--- user/add.sh
+++ user/add.sh
@@ -1352,6 +1352,7 @@ sed -i -e "s|USERNAME=\"[^\"]*\"|USERNAME=\"$username\"|g" \
     -e "s|PGADMIN_PORT=\"[^\"]*\"|PGADMIN_PORT=\"$port_1\"|g" \
     -e "s|POSTGRES_PORT=\"[^\"]*\"|POSTGRES_PORT=\"127.0.0.1:$port_3\"|g" \
     -e "s|PMA_PORT=\"[^\"]*\"|PMA_PORT=\"$port_4\"|g" \
+	-e "s|{PMA_PORT}|$FOURTH_NEXT_AVAILABLE|g" \
     -e "s|POSTGRES_PASSWORD=\"[^\"]*\"|POSTGRES_PASSWORD=\"$postgres_password\"|g" \
     -e "s|PGADMIN_PW=[^\"]*|PGADMIN_PW=$pg_admin_password|g" \
     -e "s|OPENSEARCH_INITIAL_ADMIN_PASSWORD=\"[^\"]*\"|OPENSEARCH_INITIAL_ADMIN_PASSWORD=\"$pg_admin_password\"|g" \
