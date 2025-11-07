b70fe1be20434cff0ecef2e11cfec276481cebd6
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-11-07 15:47:45 +0100

    Update faq.sh

--- faq.sh
+++ faq.sh
@@ -71,7 +71,7 @@ get_force_domain() {
 }
 
 get_public_ip() {
-    ip=$(curl --silent --max-time 2 -4 $IP_SERVER_1 || wget --timeout=2 -qO- $IP_SERVER_2 || curl --silent --max-time 2 -4 $IP_SERVER_3)
+    ip=$(curl --silent --max-time 2 -4 $IP_SERVER_1 || wget --timeout=2 --tries=1 -qO- $IP_SERVER_2 || curl --silent --max-time 2 -4 $IP_SERVER_3)
     if [ -z "$ip" ] || ! [[ "$ip" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
         ip=$(hostname -I | awk '{print $1}')
     fi
