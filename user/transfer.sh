e497bc6c120356b672e2fb459dcd0e5645e6aa14
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-11-07 15:50:21 +0100

    Update transfer.sh

--- user/transfer.sh
+++ user/transfer.sh
@@ -183,7 +183,7 @@ get_server_ipv4(){
 	IP_SERVER_3="https://ifconfig.me"
 
 	current_ip=$(curl --silent --max-time 2 -4 $IP_SERVER_1 || \
-                 wget --inet4-only --timeout=2 -qO- $IP_SERVER_2 || \
+                 wget --timeout=2 --tries=1 --inet4-only -qO- $IP_SERVER_2 || \
                  curl --silent --max-time 2 -4 $IP_SERVER_3)
 
 	if [ -z "$current_ip" ]; then
