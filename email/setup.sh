cddda63bab79e69b557c2e9bd3edcd657c37a4fc
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-02-12 12:03:34 +0100

    Update setup.sh

--- email/setup.sh
+++ email/setup.sh
@@ -96,7 +96,8 @@ reload_emails_data_file_for_user() {
 validate_first
 command="$@" 
 docker exec openadmin_mailserver setup $command  
-if [[ "$1" == "email" && ("$2" == "add" || "$2" == "del") ]]; then
+if { [[ "$1" == "email" && ( "$2" == "add" || "$2" == "del" ) ]] || \
+     [[ "$1" == "quota" && ( "$2" == "set" || "$2" == "del" ) ]]; }; then
   if is_valid_email "$3"; then
     reload_emails_data_file_for_user $3
   fi
