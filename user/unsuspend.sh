eaa5e2a12d7035549c7cdddfcf8a6a6f730cc048
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-03-08 22:41:55 +0100

    Update unsuspend.sh

--- user/unsuspend.sh
+++ user/unsuspend.sh
@@ -111,6 +111,7 @@ rename_user_in_db() {
         echo "ERROR: Failed to unsuspend user '$USERNAME'."
         exit 1
     fi
+    setsid -f opencli sentinel --action=user_status --title="User account unsuspended" --message="User account '$USERNAME' has been unsuspended." >/dev/null 2>&1
 }
 
 
