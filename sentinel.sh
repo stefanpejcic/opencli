786607a5e2db451bbf08fb7c1d6724f09cb3c165
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-03-09 20:32:02 +0100

    Update sentinel.sh

--- sentinel.sh
+++ sentinel.sh
@@ -589,6 +589,10 @@ run_action() {
   action="$1"
   local title="$2"
   local message="$3"
+
+  [[ -n "$RUN_ACTION_LOCKED" ]] && return
+  export RUN_ACTION_LOCKED=1
+  
   ACTION=$(validate_yes_no "$(ini_get $action)")
   if [[ "$ACTION" == "no" ]]; then
     echo "[!] Notifications are disabled for action: $action"; return
