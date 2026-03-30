6baa61b4f15a4b57a0f49730345413b7857c6b6e
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-03-30 12:42:17 +0200

    Update delete.sh

--- user/delete.sh
+++ user/delete.sh
@@ -136,8 +136,12 @@ postfwd_setup(){
 delete_email_users() {
     openpanel_username="$1"
     local email_file="/etc/openpanel/openpanel/core/users/$openpanel_username/emails.yml"
-    if [ -f "$email_file" ]; then    
-        mapfile -t emails < <(awk 'NF {print $2}' "$email_file")
+    if [ -f "$email_file" ]; then
+        emails=()
+        while read -r _ email; do
+            [ -n "$email" ] && emails+=("$email")
+        done < "$email_file"
+
         if [ "${#emails[@]}" -gt 0 ]; then
             opencli email-setup email del -y "${emails[@]}"
         fi
