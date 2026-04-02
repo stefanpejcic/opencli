4da95a6b576ed1e8326c8d2b22f81a893f8dbe37
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-04-02 15:35:37 +0200

    Update setup.sh

--- email/setup.sh
+++ email/setup.sh
@@ -76,6 +76,7 @@ reload_emails_data_file_for_user() {
     if [ -n "$owner" ]; then
         file_to_refresh="/etc/openpanel/openpanel/core/users/$owner/emails.yml"
         ALL_DOMAINS_OWNED_BY_USER=$(opencli domains-user $owner)
+        sleep 0.5
         ALL_EMAILS_ON_SERVER=$(opencli email-setup email list)
         
         > "$file_to_refresh"
