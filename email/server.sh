577bfcfe0b1a4b9454f5dd28df8558bb239d094f
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-04-23 12:06:39 +0200

    Update server.sh

--- email/server.sh
+++ email/server.sh
@@ -7,8 +7,8 @@
 # Author: Stefan Pejcic
 # Created: 18.08.2024
 # Last Modified: 23.04.2026
-# Company: openpanel.comm
-# Copyright (c) openpanel.comm
+# Company: openpanel.com
+# Copyright (c) openpanel.com
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
