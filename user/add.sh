62917b7bab4795ad4f9efff656e75fa6ed9f77ef
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-06-13 12:24:17 +0200

    https://github.com/stefanpejcic/OpenPanel/pull/977/changes

--- user/add.sh
+++ user/add.sh
@@ -804,11 +804,11 @@ configure_environment() {
     chown -R "${USERNAME}:${USERNAME}" "${home_dir}/sockets"
     chmod 777 "${home_dir}/sockets/"
 
-    printf '[client]\nuser=root\npassword=%s\n' "$mysql_root_pw" > "${home_dir}/my.cnf"
+    printf '[client]\nuser=root\npassword=%s\n' "$root_password_for_services" > "${home_dir}/my.cnf"
     [[ -f "${home_dir}/my.cnf" ]] || echo "[!] Warning: Failed to create my.cnf."
 
     # shellcheck disable=SC2034
-    MYSQL_ROOT_PASSWORD="$mysql_root_pw"   # exported for pull_images
+    MYSQL_ROOT_PASSWORD="$root_password_for_services"   # exported for pull_images
 }
 
 copy_skeleton_files() {
