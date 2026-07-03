1e266e7bc666a1536426c8f320e886c9cfb751e9
Radovan Ječmenica <147708271+radovanjecmenica@users.noreply.github.com>
2026-07-03 14:35:41 +0200

    Update restore.sh

--- user/restore.sh
+++ user/restore.sh
@@ -60,13 +60,13 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-WORK="${WORK:-/home/restore_tmp}"
-mkdir -p "$WORK" || { echo "[ERROR] Cannot create WORK dir: $WORK"; exit 1; }
+[[ -z "$WORK" ]] && WORK=$(mktemp -d /tmp/oprestore.XXXXXX)
 [[ -n "$(ls -A "$WORK" 2>/dev/null)" ]] && { echo "[ERROR] WORK dir not empty: $WORK - remove the --temp-dir= flag to use /tmp/ instead OR create a new subdirectory (e.g. --temp-dir=/home/restore_process)"; exit 1; }
 
 [[ -z "$ARCHIVE" ]] && { echo "Usage: opencli user-restore --file <ARCHIVE> [--force] [--new-username=NAME] [--temp-dir=/home/] [--quiet]"; exit 1; }
 [[ -f "$ARCHIVE" ]] || { echo "[ERROR] Archive not found: $ARCHIVE"; exit 1; }
 ARCHIVE=$(realpath "$ARCHIVE")
+mkdir -p "$WORK" 
 
 # DB
 DB_CONFIG_FILE="/usr/local/opencli/db.sh"
