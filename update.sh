2abea027d5b0b4736f4d4d810f473f1763d8b058
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-07-21 08:52:54 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -697,7 +697,7 @@ update_openadmin() {
 
 		#remote_version=$(opencli update --check 2>/dev/null | jq -r '.latest_version' 2>/dev/null)
 		# HARDCODED FOR PODMAN!
-		remote_version="2.0.0-beta"
+		remote_version="2.0.0"
 		url="https://github.com/stefanpejcek/openadmin/releases/download/$remote_version/$admin_binary"
 
 		if curl -sSLI -o /dev/null -w "%{http_code}" "$url" | grep -q "^200$"; then
