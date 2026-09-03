5ed133f47b72c9c1fa3dd4b90143e31ebbb4b751
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-09-03 16:48:28 +0200

    Update add.sh

--- user/add.sh
+++ user/add.sh
@@ -33,7 +33,7 @@ readonly FORBIDDEN_USERNAMES_FILE="/etc/openpanel/openadmin/config/forbidden_use
 readonly DB_CONFIG_FILE="/usr/local/opencli/db.sh"
 readonly PANEL_CONFIG_FILE="/etc/openpanel/openpanel/conf/openpanel.config"
 readonly LOCK_FILE="/var/lock/openpanel_user_add.lock"
-readonly SHARED_STORE="/var/lib/containers/shared-storage" # for 2.0.0-beta: ln -s /var/lib/containers/shared-storage /var/lib/openpanel/shared-containers/storage
+readonly SHARED_STORE="/var/lib/containers/shared-storage" # for 2.0.0-beta: /var/lib/openpanel/shared-containers/storage
 
 if [[ "$#" -lt 4 || "$#" -gt 12 ]]; then
     echo "Usage: opencli user-add <username> <password|generate> <email> '<plan_name>' [--send-email] [--debug] [--reseller=<RESELLER_USER>] [--private-note=<NOTE>] [--webserver=<TYPE>] [--sql=<mysql|mariadb>] [--no-sentinel]"
